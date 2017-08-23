ec21ff4ad6d889f4ea5764cff44cbed224487534
mkuz <michailkuznetsov@gmail.com>
2017-08-23 12:10:07 +0200

    add storybook, reorganize components

---
+++ src/components/JbButton/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './JbButton.css';
+
+class JbButton extends Component {
+  render() {
+    return (
+      <button className="jb-btn btn">{this.props.text}</button>
+    );
+  }
+}
+
+export default JbButton;
