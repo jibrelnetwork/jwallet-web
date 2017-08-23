13b33e035875558b0a3669b3e88b094bbd939511
mkuz <michailkuznetsov@gmail.com>
2017-08-23 14:35:03 +0200

    adding Router

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 import JbButton from '../../components/JbButton/';
+import Demo from  '../../components/Demo/';
+import NavBar from  '../../components/NavBar/';
+import TransactionsList from  '../../components/TransactionsList/';
+import Settings from  '../../components/Settings/';
+import Login from  '../../components/Login/';
+
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React - test auto-deploy</h2>
-          <JbButton text="Test text"/>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <HashRouter>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={TransactionsList}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/settings" component={Settings}/>
+        <Route path="/list" component={TransactionsList}/>
+      </Switch>
+  </HashRouter>);
   }
 }
 
