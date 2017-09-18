ff845bcb60f2a4e9ec71a8fad3c4d40355cbaa7e
Ivan Violentov <violentoff@gmail.com>
2017-09-18 21:25:21 +0300

    Update BackupKeysModal component

--- src/components/ImportKeysModal/ImportKeysModal.js
+++ src/components/ImportKeysModal/ImportKeysModal.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import JModal from 'components/base/JModal'
+import { getFieldMessage, handleEnterKeyPress } from 'utils'
+
+import { JModal, JModalButton, JTextInput } from 'components/base'
 
 class ImportKeysModal extends Component {
   render() {
-    const { closeImportKeysModal, keys } = this.props
+    const { closeImportKeysModal, importKeys, keys } = this.props
 
     return (
       <JModal
         closeModal={closeImportKeysModal}
+        submitModal={handleEnterKeyPress(importKeys)}
         name='import-keys'
-        header={'Import Keys'}
+        alert={keys.importKeysData.alert}
+        header={this.renderHeader()}
         body={this.renderBody()}
         footer={this.renderFooter()}
         isOpen={keys.isImportKeysModalOpen}
@@ -19,13 +23,71 @@ class ImportKeysModal extends Component {
     )
   }
 
+  renderHeader = () => {
+    return <div className='modal__title'>{'Import Keys'}</div>
+  }
+
   renderBody = () => {
-    return 'Import Keys Body'
+    return (
+      <div className='import-keys__body'>
+        {this.renderData()}
+        {this.renderPincode()}
+      </div>
+    )
+  }
+
+  renderData = () => {
+    const { setImportKeysData, keys } = this.props
+
+    return (
+      <JTextInput
+        onValueChange={setImportKeysData}
+        name='data'
+        placeholder='Address, private key, mnemonic'
+        value={keys.importKeysData.data}
+        errorMessage={this.getInvalidFieldMessage('data')}
+        successMessage={this.getValidFieldMessage('data')}
+        editable={this.isEnabledField('data')}
+        multiline
+        secureTextEntry
+      />
+    )
+  }
+
+  renderPincode = () => {
+    const { setImportKeysPincode, keys } = this.props
+
+    return (
+      <JTextInput
+        onValueChange={setImportKeysPincode}
+        name='pincode'
+        placeholder='Create your pincode'
+        value={keys.importKeysData.pincode}
+        errorMessage={this.getInvalidFieldMessage('pincode')}
+        successMessage={this.getValidFieldMessage('pincode')}
+        editable={this.isEnabledField('pincode')}
+        secureTextEntry
+      />
+    )
   }
 
   renderFooter = () => {
-    return 'Import Keys Footer'
+    const { importKeys, keys } = this.props
+
+    return (
+      <JModalButton
+        onPress={importKeys}
+        name={'import-keys'}
+        iconName={'import-keys'}
+        title={'Import Keys'}
+        disabled={keys.importKeysData.invalidFields.length > 0}
+      />
+    )
   }
+
+  isEnabledField = name => (this.props.keys.importKeysData.disabledFields.indexOf(name) === -1)
+  getValidFieldMessage = name => getFieldMessage(this.props.keys.importKeysData.validFields, name)
+  getInvalidFieldMessage = n => getFieldMessage(this.props.keys.importKeysData.invalidFields, n)
 }
 
 ImportKeysModal.propTypes = {
