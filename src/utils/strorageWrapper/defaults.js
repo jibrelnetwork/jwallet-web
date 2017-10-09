06d94dad98d80e6d68e1de23b0f5805117f3e2f2
Ivan Violentov <violentoff@gmail.com>
2017-10-09 10:30:34 +0300

    Get rid of useless code

--- src/utils/strorageWrapper/defaults.js
+++
@@ -1,8 +0,0 @@
-const defaults = {
-  size: 1000,
-  storageBackend: window.localStorage,
-  defaultExpires: 1000 * 3600 * 24,
-  enableCache: true,
-}
-
-export default defaults
--- src/utils/strorageWrapper/utils.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export const uuidv4 = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      let r = Math.random() * 16 | 0, 
-  v = c == 'x' ? r : (r & 0x3 | 0x8)
-      return v.toString(16)
-    })
-  }
-  /* eslint-enable */
