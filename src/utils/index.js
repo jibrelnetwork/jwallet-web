630224d015afc434541096ba2e8e40b2ccc2ced6
Ivan Violentov <violentoff@gmail.com>
2017-11-10 20:24:54 +0300

    Update SubmitModal (disable button if there are invalid fields); update using of qrCode lib

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 export defaultNetworks from './defaultNetworks'
 export * from './errors'
-export generateQRCode from './generateQRCode'
 export getDefaultTokens from './getDefaultTokens'
 export getFieldMessage from './getFieldMessage'
 export getFormattedDateString from './getFormattedDateString'
