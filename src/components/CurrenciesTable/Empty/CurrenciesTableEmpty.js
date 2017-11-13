c79875720ab22bfa65f4bee6051388d802ecb14e
Ivan Violentov <violentoff@gmail.com>
2017-11-14 00:03:52 +0300

    Fix eslint errors

--- src/components/CurrenciesTable/Empty/CurrenciesTableEmpty.js
+++ src/components/CurrenciesTable/Empty/CurrenciesTableEmpty.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CurrenciesTableEmpty() {
   return (
     <div className='table-body table-body--curencies-empty'>
-      <div className='currencies-empty' style={{ backgroundImage: `url('/img/no-data.svg')` }}>
+      <div className='currencies-empty' style={{ backgroundImage: 'url("/img/no-data.svg")' }}>
         <div className='currencies-empty__message'>
           {'Look like there isn\'t any tokens in your account'}
         </div>
