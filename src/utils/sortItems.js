580c113f963516f34301c40552e43b458ad8c317
Ivan Violentov <violentoff@gmail.com>
2017-11-15 15:32:39 +0300

    Resolve #69

--- src/utils/sortItems.js
+++ src/utils/sortItems.js
@@ -1,17 +1,24 @@
 import sortBy from 'lodash/sortBy'
 
-export default function sortItems(items, oldField, newfield, oldDirection) {
+export default function sortItems(items, oldField, newField, oldDirection) {
   const isASC = (oldDirection === 'ASC')
-  const changeDirection = (newfield === oldField)
-  const sortedItems = sortBy([...items], newfield)
+  const changeDirection = (newField === oldField)
+
+  const lowerCaseItems = items.map((i, index) => ({
+    index,
+    [newField]: ((typeof i[newField] === 'string') ? i[newField].toLowerCase() : i[newField])
+  }))
+
+  const sortedItems = sortBy(lowerCaseItems, newField)
+  const mappedItems = sortedItems.map(({ index }) => items[index])
 
   if (changeDirection && isASC) {
-    sortedItems.reverse()
+    mappedItems.reverse()
   }
 
   return {
-    items: sortedItems,
-    sortField: newfield,
+    items: mappedItems,
+    sortField: newField,
     sortDirection: (changeDirection && isASC) ? 'DESC' : 'ASC',
   }
 }
