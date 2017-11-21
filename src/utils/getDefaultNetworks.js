db84f0935560f89077080763a3fbde37e75786d5
Ivan Violentov <violentoff@gmail.com>
2017-11-21 11:57:52 +0300

    Update networks

--- src/utils/getDefaultNetworks.js
+++ src/utils/getDefaultNetworks.js
@@ -1,23 +1,56 @@
 import i18n from 'i18n/en'
 
-const defaultProps = { isCustom: false, ssl: false }
-
 function getDefaultNetworks() {
-  const { main, ropsten /* , kovan, rinkeby */ } = i18n.networkManager.knownNetworkTitle
+  const { main, ropsten } = i18n.networkManager.knownNetworkTitle
 
-  return [
-    { title: main, rpcaddr: 'node.jwallet.network', rpcport: '8545', ...defaultProps, id: 1 },
-    { title: ropsten, rpcaddr: '37.59.55.6', rpcport: '20003', ...defaultProps, id: 3 },
-    /*
-    { title: main, rpcaddr: '138.197.180.127', rpcport: '20001', ...defaultProps },
-    { title: ropsten, rpcaddr: '37.59.55.6', rpcport: '20003', ...defaultProps },
-    */
-    /*
-    { title: kovan, rpcaddr: '37.59.55.6', rpcport: '10002', ...defaultProps },
-    { title: rinkeby, rpcaddr: '37.59.55.6', rpcport: '10004', ...defaultProps },
-    */
-    { title: 'Localhost 8545', rpcaddr: 'localhost', rpcport: '8545', ...defaultProps, id: 0 },
-  ]
+  return [{
+    id: 1,
+    title: main,
+    rpcaddr: 'node.jwallet.network',
+    rpcport: '8545',
+    ssl: true,
+    isCustom: false,
+  }, {
+    id: 3,
+    title: ropsten,
+    rpcaddr: 'ns3089219.ip-37-59-55.eu',
+    rpcport: '20003', // parity node
+    ssl: true,
+    isCustom: false,
+  }, /* {
+    id: 1,
+    title: main,
+    rpcaddr: '138.197.180.127',
+    rpcport: '20001',
+    ssl: true,
+    isCustom: false,
+  }, {
+    id: 3,
+    title: ropsten,
+    rpcaddr: '37.59.55.6',
+    rpcport: '10003', // geth node
+    ssl: true,
+    isCustom: false,
+  }, */ /* {
+    title: kovan,
+    rpcaddr: '37.59.55.6',
+    rpcport: '10002',
+    ssl: true,
+    isCustom: false,
+  }, {
+    title: rinkeby,
+    rpcaddr: '37.59.55.6',
+    rpcport: '10004',
+    ssl: true,
+    isCustom: false,
+  }, */ {
+    id: 0,
+    title: 'Localhost 8545',
+    rpcaddr: 'localhost',
+    rpcport: '8545',
+    ssl: true,
+    isCustom: false,
+  }]
 }
 
 export default getDefaultNetworks
