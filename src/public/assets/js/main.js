30ebdae7b51971ac2c87180b1513272d3a1e3098
Ivan Violentov <violentoff@gmail.com>
2017-11-22 21:13:02 +0300

    Fix ES6 syntax on home page; add version param for token icons

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -98,45 +98,45 @@ $(document).ready(function() {
     // https://github.com/glslify/glsl-easings
     //
 
-    const ease = {
-        exponentialIn: (t) => {
+    var ease = {
+        exponentialIn: function(t) {
             return t == 0.0 ? t : Math.pow(2.0, 10.0 * (t - 1.0));
         },
-        exponentialOut: (t) => {
+        exponentialOut: function(t) {
             return t == 1.0 ? t : 1.0 - Math.pow(2.0, -10.0 * t);
         },
-        exponentialInOut: (t) => {
+        exponentialInOut: function(t) {
             return t == 0.0 || t == 1.0
                 ? t
                 : t < 0.5
                     ? +0.5 * Math.pow(2.0, (20.0 * t) - 10.0)
                     : -0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
         },
-        sineOut: (t) => {
+        sineOut: function(t) {
             const HALF_PI = 1.5707963267948966;
             return Math.sin(t * HALF_PI);
         },
-        circularInOut: (t) => {
+        circularInOut: function(t) {
             return t < 0.5
                 ? 0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t))
                 : 0.5 * (Math.sqrt((3.0 - 2.0 * t) * (2.0 * t - 1.0)) + 1.0);
         },
-        cubicIn: (t) => {
+        cubicIn: function(t) {
             return t * t * t;
         },
-        cubicOut: (t) => {
+        cubicOut: function(t) {
             const f = t - 1.0;
             return f * f * f + 1.0;
         },
-        cubicInOut: (t) => {
+        cubicInOut: function(t) {
             return t < 0.5
                 ? 4.0 * t * t * t
                 : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
         },
-        quadraticOut: (t) => {
+        quadraticOut: function(t) {
             return -t * (t - 2.0);
         },
-        quarticOut: (t) => {
+        quarticOut: function(t) {
             return Math.pow(t - 1.0, 3.0) * (1.0 - t) + 1.0;
         },
     }
