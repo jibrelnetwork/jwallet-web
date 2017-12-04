841921fdb2535961ffae98475b511e6ade65cc6d
Ivan Violentov <violentoff@gmail.com>
2017-12-04 19:28:32 +0300

    Update using of geth ropsten node

--- src/utils/getDefaultNetworks.js
+++ src/utils/getDefaultNetworks.js
@@ -12,28 +12,28 @@ function getDefaultNetworks() {
     // rpcport: '8545',
     ssl: true,
     isCustom: false,
-  }, {
+  }, /* {
     id: 3,
     title: ropsten,
     rpcaddr: 'ns3089219.ip-37-59-55.eu',
     rpcport: '20003', // parity node
     ssl: true,
     isCustom: false,
-  }, /* {
+  }, {
     id: 1,
     title: main,
     rpcaddr: '138.197.180.127',
     rpcport: '20001', // geth node
     ssl: true,
     isCustom: false,
-  }, {
+  }, */ {
     id: 3,
     title: ropsten,
-    rpcaddr: '37.59.55.6',
+    rpcaddr: 'ns3089219.ip-37-59-55.eu',
     rpcport: '10003', // geth node
     ssl: true,
     isCustom: false,
-  }, */ /* {
+  }, /* {
     title: kovan,
     rpcaddr: '37.59.55.6',
     rpcport: '10002',
