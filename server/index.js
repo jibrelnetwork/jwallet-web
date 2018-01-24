d92671dc6c50e06fca0b3e8c9bbf17e8c9e184cd
Ivan Violentov <violentoff@gmail.com>
2018-01-24 15:10:59 +0300

    Get rid of landing page (move it to another repository)

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const _debug = require('debug')
-const server = require('./server')
-
-const debug = _debug('app:bin:server')
-const port = process.env.PORT || 3000
-const host = process.env.HOST || 'localhost'
-
-server.listen(port)
-debug(`Server is now running at http://${host}:${port}.`)
