b8f95506e90015baf829505a9c271538f00de86f
Ivan Violentov <violentoff@gmail.com>
2018-01-31 18:12:06 +0300

    Update flow-typed (declared types & functions)

---
+++ flow-typed/types.js
@@ -0,0 +1,22 @@
+declare type Address = string
+
+declare type DigitalAsset = {
+  address: string,
+  symbol: string,
+  name: string,
+  decimals: number,
+  isAuthRequired: boolean,
+  isLicensed: boolean,
+  isCurrent: boolean,
+  isCustom: boolean,
+  isActive: boolean,
+}
+
+declare type DigitalAssets = Array<DigitalAsset>
+
+declare type TokenData = {
+  address: string,
+  symbol: string,
+  name: string,
+  decimals: string,
+}
