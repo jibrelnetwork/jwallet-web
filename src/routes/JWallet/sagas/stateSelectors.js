c62db3c730461a98025e1f39d069f38b4bdb7dff
Ivan Violentov <violentoff@gmail.com>
2018-02-01 12:44:22 +0300

    Update flow for sagas

--- src/routes/JWallet/sagas/stateSelectors.js
+++ src/routes/JWallet/sagas/stateSelectors.js
@@ -1,74 +1,76 @@
+// @flow
+
 import find from 'lodash/find'
 
 import isMnemonicType from 'utils/isMnemonicType'
 
-export function selectDigitalAssets({ currencies }) {
-  return currencies
+export function selectDigitalAssets(state: { currencies: any }) {
+  return state.currencies
 }
 
-export function selectCurrentDigitalAsset({ currencies }) {
-  return find(currencies.items, { address: currencies.currentAddress })
+export function selectCurrentDigitalAsset(state: { currencies: any }) {
+  return find(state.currencies.items, { address: state.currencies.currentAddress })
 }
 
-export function selectSendFundsModal({ sendFundsModal }) {
-  return sendFundsModal
+export function selectSendFundsModal(state: { sendFundsModal: any }) {
+  return state.sendFundsModal
 }
 
-export function selectCurrentKeystoreAddress({ keystore }) {
-  const { currentAccount, addressesFromMnemonic } = keystore
+export function selectCurrentKeystoreAddress(state: { keystore: any }) {
+  const { currentAccount, addressesFromMnemonic } = state.keystore
   const { type, address, addressIndex } = currentAccount
 
   return isMnemonicType(type) ? addressesFromMnemonic.items[addressIndex] : address
 }
 
-export function selectNetworks({ networks }) {
-  return networks
+export function selectNetworks(state: { networks: any }) {
+  return state.networks
 }
 
-export function selectCurrentNetwork({ networks }) {
-  return networks.items[networks.currentNetworkIndex]
+export function selectCurrentNetwork(state: { networks: any }) {
+  return state.networks.items[state.networks.currentNetworkIndex]
 }
 
-export function selectCurrentNetworkId({ networks }) {
-  return `${networks.items[networks.currentNetworkIndex].id}`
+export function selectCurrentNetworkId(state: { networks: any }) {
+  return `${state.networks.items[state.networks.currentNetworkIndex].id}`
 }
 
-export function selectKeystoreData({ keystore }) {
-  return keystore
+export function selectKeystoreData(state: { keystore: any }) {
+  return state.keystore
 }
 
-export function selectCurrentAccount({ keystore }) {
-  return keystore.currentAccount
+export function selectCurrentAccount(state: { keystore: any }) {
+  return state.keystore.currentAccount
 }
 
-export function selectCurrentAccountId({ keystore }) {
-  return keystore.currentAccount.id
+export function selectCurrentAccountId(state: { keystore: any }) {
+  return state.keystore.currentAccount.id
 }
 
-export function selectTransactions({ transactions }) {
-  return transactions
+export function selectTransactions(state: { transactions: any }) {
+  return state.transactions
 }
 
-export function selectClearKeystoreData({ clearKeystoreModal }) {
-  return clearKeystoreModal
+export function selectClearKeystoreData(state: { clearKeystoreModal: any }) {
+  return state.clearKeystoreModal
 }
 
-export function selectImportAccountModalData({ importKeystoreAccountModal }) {
-  return importKeystoreAccountModal
+export function selectImportAccountModalData(state: { importKeystoreAccountModal: any }) {
+  return state.importKeystoreAccountModal
 }
 
-export function selectNewAccountModalData({ newKeystoreAccountModal }) {
-  return newKeystoreAccountModal
+export function selectNewAccountModalData(state: { newKeystoreAccountModal: any }) {
+  return state.newKeystoreAccountModal
 }
 
-export function selectNewDerivationPathModalData({ newDerivationPathModal }) {
-  return newDerivationPathModal
+export function selectNewDerivationPathModalData(state: { newDerivationPathModal: any }) {
+  return state.newDerivationPathModal
 }
 
-export function selectAlphaWarningModalData({ alphaWarningModal }) {
-  return alphaWarningModal
+export function selectAlphaWarningModalData(state: { alphaWarningModal: any }) {
+  return state.alphaWarningModal
 }
 
-export function selectBackupKeystoreModalData({ backupKeystoreModal }) {
-  return backupKeystoreModal
+export function selectBackupKeystoreModalData(state: { backupKeystoreModal: any }) {
+  return state.backupKeystoreModal
 }
