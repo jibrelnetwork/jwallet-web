08ac917da7ea32498d66ad67ba928487801d5b1f
Ivan Violentov <violentoff@gmail.com>
2018-02-02 14:04:57 +0300

    Move ChangePassword sagas & modules to appropriate route dir

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -9,6 +9,7 @@ import * as networksSagas from 'routes/JWallet/sagas/networks'
 import * as transactionsSagas from 'routes/JWallet/sagas/transactions'
 import * as addCustomAssetSagas from 'routes/AddCustomAsset/sagas/addCustomAsset'
 import * as backupKeysSagas from 'routes/BackupKeys/sagas/backupKeys'
+import * as changePasswordSagas from 'routes/ChangePassword/sagas/changePassword'
 
 export default {
   ...alphaWarningModalSagas,
@@ -22,4 +23,5 @@ export default {
   ...transactionsSagas,
   ...addCustomAssetSagas,
   ...backupKeysSagas,
+  ...changePasswordSagas,
 }
