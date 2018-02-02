882edd9c44428b7b4f560976018a28976569b180
Ivan Violentov <violentoff@gmail.com>
2018-02-02 17:12:41 +0300

    Update ClearKeys component

--- src/routes/ClearKeys/components/ClearKeys.js
+++ src/routes/ClearKeys/components/ClearKeys.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const ClearKeys = () => (
+import JTextInput from 'components/base/JTextInput'
+
+const ClearKeys = ({ setPassword, invalidFields, password }) => (
   <div className='clear-keys-view'>
-    {'ClearKeys view'}
+    <JTextInput
+      onValueChange={setPassword}
+      name='clear-keys-password'
+      placeholder='Password'
+      value={password}
+      errorMessage={invalidFields.password}
+      editable
+    />
   </div>
 )
+
+ClearKeys.propTypes = {
+  setPassword: PropTypes.func.isRequired,
+  // clear: PropTypes.func.isRequired,
+  invalidFields: PropTypes.shape({}).isRequired,
+  password: PropTypes.string.isRequired,
+}
 
 export default ClearKeys
--- src/routes/ClearKeys/containers/ClearKeysContainer.js
+++ src/routes/ClearKeys/containers/ClearKeysContainer.js
@@ -1,3 +1,17 @@
+import { connect } from 'react-redux'
+
+import {
+  setPassword,
+  // clear,
+} from '../modules/clearKeys'
+
 import ClearKeys from '../components/ClearKeys'
 
-export default ClearKeys
+const mapStateToProps = ({ clearKeys }) => clearKeys
+
+const mapDispatchToProps = {
+  setPassword,
+  // clear,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClearKeys)
