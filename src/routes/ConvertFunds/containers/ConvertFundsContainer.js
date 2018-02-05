c16acff14fcfbe1cb240f528e50e009fa90f9b52
Ivan Violentov <violentoff@gmail.com>
2018-02-05 14:15:24 +0300

    Update ConvertFunds route component

--- src/routes/ConvertFunds/containers/ConvertFundsContainer.js
+++ src/routes/ConvertFunds/containers/ConvertFundsContainer.js
@@ -1,3 +1,21 @@
+import { connect } from 'react-redux'
+
+import {
+  setFromAsset,
+  setFromAmount,
+  setToAsset,
+  // convert,
+} from '../modules/convertFunds'
+
 import ConvertFunds from '../components/ConvertFunds'
 
-export default ConvertFunds
+const mapStateToProps = ({ convertFunds }) => convertFunds
+
+const mapDispatchToProps = {
+  setFromAsset,
+  setFromAmount,
+  setToAsset,
+  // convert,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConvertFunds)
