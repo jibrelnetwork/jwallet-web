903c249dbae23c1395178d3f7a1d15c4269a9356
Ivan Violentov <violentoff@gmail.com>
2018-02-07 12:59:27 +0300

    Remove checking of password in ClearKeystore modal

--- src/components/modals/ClearKeystore/ClearKeystoreModal.js
+++ src/components/modals/ClearKeystore/ClearKeystoreModal.js
@@ -5,29 +5,18 @@ import SubmitModal from 'components/SubmitModal'
 class ClearKeystoreModal extends SubmitModal {
   renderModalBody = () => ''
   closeModal = () => this.props.closeClearKeystoreModal()
-  setPassword = password => this.props.setClearKeystorePassword(password)
-  submitModal = () => this.props.removeKeystoreAccounts(this.props.password)
+  submitModal = () => this.props.removeKeystoreAccounts()
 }
 
 ClearKeystoreModal.propTypes = {
   closeClearKeystoreModal: PropTypes.func.isRequired,
-  setClearKeystorePassword: PropTypes.func.isRequired,
   removeKeystoreAccounts: PropTypes.func.isRequired,
-  invalidFields: PropTypes.shape({}).isRequired,
   alert: PropTypes.string.isRequired,
   modalName: PropTypes.string.isRequired,
   modalTitle: PropTypes.string.isRequired,
   buttonTitle: PropTypes.string.isRequired,
-  buttonType: PropTypes.string.isRequired,
   imageName: PropTypes.string.isRequired,
   isOpen: PropTypes.bool.isRequired,
-  /* optional */
-  onClose: PropTypes.func,
-}
-
-ClearKeystoreModal.defaultProps = {
-  ...SubmitModal.defaultProps,
-  onClose: () => {},
 }
 
 export default ClearKeystoreModal
