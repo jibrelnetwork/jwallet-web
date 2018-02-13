41599901730b4792eaf567e6c7d50da0cd264e34
Ivan Violentov <violentoff@gmail.com>
2018-02-13 20:44:46 +0300

    Update utils functions

---
+++ src/utils/getTransactionValue.js
@@ -0,0 +1,18 @@
+// @flow
+
+import toBigNumber from './toBigNumber'
+
+const getTransactionValue = (
+  amount: string | number,
+  decimals: number
+): Bignumber => {
+  const value: Bignumber = toBigNumber(amount)
+  const units: Bignumber = toBigNumber(10)
+
+  const decimalsPower: Bignumber = units.pow(decimals)
+  const fixedValue: string = value.times(decimalsPower).toFixed()
+
+  return toBigNumber(fixedValue)
+}
+
+export default getTransactionValue
