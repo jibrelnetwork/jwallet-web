86528b11cf75a2b13dcb584d324427a3400709f3
Ivan Violentov <violentoff@gmail.com>
2018-02-16 19:41:43 +0300

    Add validateKeyName & validateDerivationPath util methods

---
+++ src/utils/validateDerivationPath.js
@@ -0,0 +1,23 @@
+// @flow
+
+import Keystore from 'jwallet-web-keystore'
+
+import { InvalidFieldError } from './errors'
+
+type DerivationPathData = {
+  customDerivationPath: string,
+  knownDerivationPath: string,
+}
+
+const validateDerivationPath = (data: DerivationPathData): void => {
+  const derivationPath: string = data.customDerivationPath || data.knownDerivationPath
+
+  if (!Keystore.isDerivationPathValid(derivationPath)) {
+    throw new InvalidFieldError(
+      'customDerivationPath',
+      i18n('general.error.derivationPath.invalid'),
+    )
+  }
+}
+
+export default validateDerivationPath
