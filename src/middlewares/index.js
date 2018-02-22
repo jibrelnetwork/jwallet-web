a027f3eaaf5979baac10d97ab64eb1d66c8742d6
Ivan Violentov <violentoff@gmail.com>
2018-02-22 23:22:10 +0300

    Add storage middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 import * as forms from './forms'
 import * as router from './router'
+import * as storage from './storage'
 import * as analytics from './analytics'
 import * as notification from './notification'
 
 export default [
+  storage.set,
   router.redirect,
   notification.show,
   analytics.pushEvent,
