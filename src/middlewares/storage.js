2f76a58d307b430682ffb8b06eb31c106b58bb33
Ivan Violentov <violentoff@gmail.com>
2018-02-26 16:59:36 +0300

    Add updating of keystore in storage after removing of wallet

--- src/middlewares/storage.js
+++ src/middlewares/storage.js
@@ -1,13 +1,15 @@
 // @flow
 
 import { keystore, storage } from 'services'
+import * as removeWallet from 'routes/Wallets/routes/RemoveWallet/modules/removeWallet'
 import * as changeWalletPassword from 'routes/Wallets/routes/ChangeWalletPassword/modules/changeWalletPassword' // eslint-disable-line max-len
 
 export const set = () => (next: Next) => (action: FSA) => {
   const { type }: FSA = action
 
   switch (type) {
-    case changeWalletPassword.CHANGE_PASSWORD_SUCCESS: {
+    case changeWalletPassword.CHANGE_PASSWORD_SUCCESS:
+    case removeWallet.REMOVE_SUCCESS: {
       storage.setKeystore(keystore.serialize())
       break
     }
