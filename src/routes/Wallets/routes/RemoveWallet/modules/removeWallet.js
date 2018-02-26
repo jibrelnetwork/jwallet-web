d3f26f0e1c6fc103031d4862fb1b81fbe38a379e
Ivan Violentov <violentoff@gmail.com>
2018-02-26 23:10:13 +0300

    Update removeWallet module

--- src/routes/Wallets/routes/RemoveWallet/modules/removeWallet.js
+++ src/routes/Wallets/routes/RemoveWallet/modules/removeWallet.js
@@ -6,23 +6,17 @@ export const REMOVE = '@@removeWallet/REMOVE'
 export const REMOVE_SUCCESS = '@@removeWallet/REMOVE_SUCCESS'
 export const REMOVE_ERROR = '@@removeWallet/REMOVE_ERROR'
 
-export const open = (): { type: string } => {
-  return {
-    type: OPEN,
-  }
-}
+export const open = (): { type: string } => ({
+  type: OPEN,
+})
 
-export const close = (): { type: string } => {
-  return {
-    type: CLOSE,
-  }
-}
+export const close = (): { type: string } => ({
+  type: CLOSE,
+})
 
-export const remove = (): { type: string } => {
-  return {
-    type: REMOVE,
-  }
-}
+export const remove = (): { type: string } => ({
+  type: REMOVE,
+})
 
 export const removeSuccess = (walletType: WalletType): {
   type: string,
