a666e52fbb3b6cb92c8809f6bd378cd9fe742b9d
erohin <erohin.em@gmail.com>
2018-02-27 21:24:41 +0300

    Refactor base components export file
    For VSCode IntelliSense support

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,23 +1,49 @@
-export JButton from './JButton'
-export JCheckbox from './JCheckbox'
-export JDropdown from './JDropdown'
-export JFooter from './JFooter'
-export JFormField from './JFormField'
-export JHeader from './JHeader'
-export JIcon from './JIcon'
-export JLoader from './JLoader'
-export JLogo from './JLogo'
-export JModal from './JModal'
-export JPicker from './JPicker'
-export JPopover from './JPopover'
-export JRadio from './JRadio'
-export JTable from './JTable'
-export JTextInput from './JTextInput'
-export JText from './JText'
-export JToast from './JToast'
-export JSelect from './JSelect'
-export JCard from './JCard'
-export JThumbnail from './JThumbnail'
-export JCallout from './JCallout'
-export JPaginator from './JPaginator'
-export JTooltip from './JTooltip'
+import JText from './JText'
+import JCard from './JCard'
+import JIcon from './JIcon'
+import JLogo from './JLogo'
+import JModal from './JModal'
+import JRadio from './JRadio'
+import JTable from './JTable'
+import JToast from './JToast'
+import JHeader from './JHeader'
+import JFooter from './JFooter'
+import JButton from './JButton'
+import JLoader from './JLoader'
+import JPicker from './JPicker'
+import JSelect from './JSelect'
+import JPopover from './JPopover'
+import JCallout from './JCallout'
+import JTooltip from './JTooltip'
+import JCheckbox from './JCheckbox'
+import JDropdown from './JDropdown'
+import JFormField from './JFormField'
+import JTextInput from './JTextInput'
+import JThumbnail from './JThumbnail'
+import JPaginator from './JPaginator'
+
+export {
+  JText,
+  JCard,
+  JIcon,
+  JLogo,
+  JRadio,
+  JTable,
+  JModal,
+  JToast,
+  JHeader,
+  JSelect,
+  JFooter,
+  JLoader,
+  JPicker,
+  JButton,
+  JCallout,
+  JPopover,
+  JTooltip,
+  JCheckbox,
+  JDropdown,
+  JPaginator,
+  JTextInput,
+  JThumbnail,
+  JFormField,
+}
