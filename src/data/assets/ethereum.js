7c209fd0f45f0a8c9cf3258a0fdad9f2fbbf8219
Ivan Violentov <violentoff@gmail.com>
2018-03-05 21:46:14 +0300

    Move popular digital assets to /src/data dir

---
+++ src/data/assets/ethereum.js
@@ -0,0 +1,10 @@
+const ethereum: DigitalAsset = {
+  name: 'Ethereum',
+  symbol: 'ETH',
+  decimals: 18,
+  address: 'Ethereum',
+  isActive: true,
+  isCustom: false,
+}
+
+export default ethereum
---
+++ src/utils/getPopularDigitalAssets.js
@@ -0,0 +1,18 @@
+// @flow
+
+import { prepend } from 'ramda'
+
+import assets from 'data/assets'
+
+const networksNamesMap: { [string]: string } = {
+  '1': 'main',
+}
+
+const getPopularDigitalAssets = (networkId: NetworkId): DigitalAssets => {
+  const networkName: string = networksNamesMap[`${networkId}`]
+  const popularAssets: DigitalAssets = assets[networkName] || []
+
+  return prepend(assets.ethereum)(popularAssets)
+}
+
+export default getPopularDigitalAssets
