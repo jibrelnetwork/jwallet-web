e5dce130272a1fd8ace7f6d879c4abe9e90e79cd
erohin <erohin.em@gmail.com>
2018-03-06 14:32:30 +0300

    Add JButton tests #223

---
+++ src/components/base/JButton/JButton.test.js
@@ -0,0 +1,12 @@
+/* @flow */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import JButton from './JButton'
+
+describe('COMPONENT: JButton', () => {
+  test('Blue', () => {
+    expect(renderer.create(<JButton color={'blue'}/>).toJSON()).toMatchSnapshot()
+  })
+})
