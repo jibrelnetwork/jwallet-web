d185941d14ab5cf1c26cd81edc51ad14d13f85d0
Ivan Violentov <violentoff@gmail.com>
2018-03-06 14:47:56 +0300

    Update convertFunds module

--- src/routes/Funds/routes/ConvertFunds/modules/convertFunds.js
+++ src/routes/Funds/routes/ConvertFunds/modules/convertFunds.js
@@ -1,5 +1,7 @@
 // @flow
 
+import { assoc } from 'ramda'
+
 export const SET_FROM_ASSET = '@@convertFunds/SET_FROM_ASSET'
 export const SET_FROM_AMOUNT = '@@convertFunds/SET_FROM_AMOUNT'
 export const SET_TO_ASSET = '@@convertFunds/SET_TO_ASSET'
@@ -7,54 +9,59 @@ export const SET_TO_AMOUNT = '@@convertFunds/SET_TO_AMOUNT'
 export const CLEAN = '@@convertFunds/CLEAN'
 export const CONVERT = '@@convertFunds/CONVERT'
 
-export function setFromAsset(address: Address) {
-  return {
-    type: SET_FROM_ASSET,
+export const setFromAsset = (address: Address): {
+  type: string,
+  payload: {
+    address: Address,
+  },
+} => ({
+  type: SET_FROM_ASSET,
+  payload: {
     address,
-  }
-}
+  },
+})
 
-export function setFromAmount(amount: string) {
-  return {
-    type: SET_FROM_AMOUNT,
+export const setFromAmount = (amount: string): {
+  type: string,
+  payload: {
+    amount: string,
+  },
+} => ({
+  type: SET_FROM_AMOUNT,
+  payload: {
     amount,
-  }
-}
+  },
+})
 
-export function setToAsset(address: Address) {
-  return {
-    type: SET_TO_ASSET,
+export const setToAsset = (address: Address): {
+  type: string,
+  payload: {
+    address: Address,
+  },
+} => ({
+  type: SET_TO_ASSET,
+  payload: {
     address,
-  }
-}
+  },
+})
 
-export function convert() {
-  return {
-    type: CONVERT,
-  }
-}
+export const setToAmount = (amount: string): {
+  type: string,
+  payload: {
+    amount: string,
+  },
+} => ({
+  type: SET_TO_AMOUNT,
+  payload: {
+    amount,
+  },
+})
 
-const ACTION_HANDLERS = {
-  [SET_FROM_ASSET]: (state, action) => ({
-    ...state,
-    fromAsset: action.address,
-  }),
-  [SET_FROM_AMOUNT]: (state, action) => ({
-    ...state,
-    fromAmount: action.amount,
-  }),
-  [SET_TO_ASSET]: (state, action) => ({
-    ...state,
-    toAsset: action.address,
-  }),
-  [SET_TO_AMOUNT]: (state, action) => ({
-    ...state,
-    toAmount: action.amount,
-  }),
-  [CLEAN]: () => initialState,
-}
+export const convert = (): { type: string } => ({
+  type: CONVERT,
+})
 
-const initialState = {
+const initialState: ConvertFundsData = {
   invalidFields: {},
   fromAsset: 'ETH',
   fromAmount: '',
@@ -62,8 +69,33 @@ const initialState = {
   toAmount: '',
 }
 
-export default function convertFunds(state: any = initialState, action: any) {
-  const handler = ACTION_HANDLERS[action.type]
+const convertFunds = (
+  state: ConvertFundsData = initialState,
+  action: FSA,
+): Object => {
+  const { type, payload }: FSA = action
 
-  return handler ? handler(state, action) : state
+  switch (type) {
+    case SET_FROM_ASSET: {
+      return assoc('fromAsset', payload.address)(state)
+    }
+
+    case SET_FROM_AMOUNT: {
+      return assoc('fromAmount', payload.amount)(state)
+    }
+
+    case SET_TO_ASSET: {
+      return assoc('toAsset', payload.address)(state)
+    }
+
+    case SET_TO_AMOUNT: {
+      return assoc('toAmount', payload.amount)(state)
+    }
+
+    case CLEAN: return initialState
+
+    default: return state
+  }
 }
+
+export default convertFunds
