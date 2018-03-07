ab35afd95cbba13a689624664f72797ecb96933d
Ivan Violentov <violentoff@gmail.com>
2018-03-07 19:40:52 +0300

    Update /wallets/start page

--- src/routes/Wallets/routes/Start/components/Start.js
+++ src/routes/Wallets/routes/Start/components/Start.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Start = () => (
   <div className='start-view'>
-    {'Start view'}
+    <div style={{ marginBottom: '40px' }}>
+      {'Create a new key pair or import anexisting one to get started'}
+    </div>
+    <Link to='/wallets/create' style={{ display: 'block', marginBottom: '10px' }}>
+      {'Create new wallet'}
+    </Link>
+    <Link to='/wallets/import'>
+      {'Import wallet'}
+    </Link>
   </div>
 )
 
