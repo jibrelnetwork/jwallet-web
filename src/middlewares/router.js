99f22fa03448e82584ef182a83fbd67c1e4ac2cf
Ivan Violentov <violentoff@gmail.com>
2018-03-08 13:51:51 +0300

    Update router middleware

--- src/middlewares/router.js
+++ src/middlewares/router.js
@@ -1,7 +1,9 @@
 // @flow
 
+import { isEmpty } from 'ramda'
 import { push } from 'react-router-redux'
 
+import keystore from 'services/keystore'
 import isMnemonicType from 'utils/isMnemonicType'
 
 /**
@@ -19,6 +21,7 @@ import * as addCustomAsset from 'routes/AddCustomAsset/modules/addCustomAsset'
  * Wallets
  */
 import * as wallets from 'routes/Wallets/modules/wallets'
+import * as mnemonicAddresses from 'routes/Wallets/routes/Addresses/modules/mnemonicAddresses'
 import * as editWallet from 'routes/Wallets/routes/EditWallet/modules/editWallet'
 import * as backupWallet from 'routes/Wallets/routes/BackupWallet/modules/backupWallet'
 import * as changeWalletPassword from 'routes/Wallets/routes/ChangeWalletPassword/modules/changeWalletPassword' // eslint-disable-line max-len
@@ -26,6 +29,7 @@ import * as removeWallet from 'routes/Wallets/routes/RemoveWallet/modules/remove
 
 export const redirect = (store: Store) => (next: Next) => (action: FSA) => {
   const { type, payload }: FSA = action
+  const goToLocation = (location: string): void => store.dispatch(push(location))
 
   switch (type) {
     /**
@@ -34,7 +38,7 @@ export const redirect = (store: Store) => (next: Next) => (action: FSA) => {
     case receiveFunds.CLOSE:
     case sendFunds.CLOSE:
     case sendFunds.SEND_SUCCESS: {
-      store.dispatch(push('/'))
+      goToLocation('/')
       break
     }
 
@@ -42,23 +46,102 @@ export const redirect = (store: Store) => (next: Next) => (action: FSA) => {
      * Digital Assets
      */
     case addCustomAsset.ADD_SUCCESS: {
-      store.dispatch(push('/'))
+      goToLocation('/')
       break
     }
 
     /**
      * Wallets
      */
-    case editWallet.OPEN:
-    case backupWallet.OPEN: {
+    case wallets.OPEN: {
+      try {
+        const wallets: Wallets = store.getState().wallets.items
+
+        if (isEmpty(wallets)) {
+          goToLocation('/wallets/start')
+        }
+      } catch (err) {
+        goToLocation('/wallets/start')
+      }
+
+      break
+    }
+
+    case mnemonicAddresses.OPEN: {
       try {
         const walletId: ?WalletId = store.getState().wallets.activeWalletId
 
         if (!walletId) {
-          store.dispatch(push('/wallets'))
+          goToLocation('/wallets')
+        } else {
+          const { type }: Wallet = keystore.getWallet(walletId)
+
+          if (!isMnemonicType(type)) {
+            goToLocation('/wallets')
+          }
         }
       } catch (err) {
-        store.dispatch(push('/wallets'))
+        goToLocation('/wallets')
+      }
+
+      break
+    }
+
+    case editWallet.OPEN:
+    case backupWallet.OPEN:
+    case removeWallet.OPEN: {
+      try {
+        const walletId: ?WalletId = store.getState().wallets.activeWalletId
+
+        if (!walletId) {
+          goToLocation('/wallets')
+        }
+      } catch (err) {
+        goToLocation('/wallets')
+      }
+
+      break
+    }
+
+    case changeWalletPassword.OPEN: {
+      try {
+        const walletId: ?WalletId = store.getState().wallets.activeWalletId
+
+        if (!walletId) {
+          goToLocation('/wallets')
+        } else {
+          const { isReadOnly }: Wallet = keystore.getWallet(walletId)
+
+          if (isReadOnly) {
+            goToLocation('/wallets')
+          }
+        }
+      } catch (err) {
+        goToLocation('/wallets')
+      }
+
+      break
+    }
+
+    case wallets.SET_ACTIVE_SUCCESS: {
+      if (isMnemonicType(payload.walletType)) {
+        goToLocation('/wallets/addresses')
+      } else if (payload.walletAction) {
+        goToLocation(`/wallets/${payload.walletAction}`)
+      } else {
+        goToLocation('/')
+      }
+
+      break
+    }
+
+    case mnemonicAddresses.SET_ACTIVE_SUCCESS: {
+      const { walletAction }: WalletsData = store.getState().wallets
+
+      if (walletAction) {
+        goToLocation(`/wallets/${walletAction}`)
+      } else {
+        goToLocation('/')
       }
 
       break
@@ -66,26 +149,13 @@ export const redirect = (store: Store) => (next: Next) => (action: FSA) => {
 
     case editWallet.EDIT_SUCCESS:
     case backupWallet.BACKUP_SUCCESS:
-    case changeWalletPassword.CLOSE:
-    case removeWallet.CLOSE: {
-      store.dispatch(push('/'))
-      break
-    }
-
-    case wallets.SET_ACTIVE_SUCCESS: {
-      if (isMnemonicType(payload.walletType)) {
-        store.dispatch(push('/wallets/addresses'))
-      } else if (payload.walletAction) {
-        store.dispatch(push(`/wallets/${payload.walletAction}`))
-      } else {
-        store.dispatch(push('/'))
-      }
-
+    case changeWalletPassword.CHANGE_PASSWORD_SUCCESS: {
+      goToLocation('/')
       break
     }
 
     case removeWallet.REMOVE_SUCCESS: {
-      store.dispatch(push('/wallets/start'))
+      goToLocation('/wallets')
       break
     }
 
