20818fa12dddcb8cf001e2f1499761ebd51605e0
erohin <erohin.em@gmail.com>
2018-03-14 17:20:35 +0300

    Add JThumbnail stories #197

---
+++ src/components/base/__new__/JThumbnail/JThumbnail.stories.js
@@ -0,0 +1,27 @@
+
+/* @flow */
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import JThumbnail from '.'
+
+storiesOf('JThumbnail', module)
+  .add('White', () => (
+    <div style={{ backgroundColor: '#0050db' }}>
+      <JThumbnail
+        image='key'
+        color='white'
+        title='Backup current key'
+        description={`All user data, including imported or generated \n private keys are stored locally, meaning your private`}
+      />
+    </div>
+  ))
+  .add('Gray', () => (
+    <JThumbnail
+      image='key'
+      color='gray'
+      title='Backup current key'
+      description={"All user data, including imported or generated \n private keys are stored locally, meaning your private"}
+    />
+  ))
