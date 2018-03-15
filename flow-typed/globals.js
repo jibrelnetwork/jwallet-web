617cf95a0e72cd0fe9fa335b456ee5b969eb1602
Ivan Violentov <violentoff@gmail.com>
2018-03-15 23:02:38 +0300

    Update flow

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -1,1 +1,5 @@
 declare var __DEV__: boolean
+declare var __MAIN_RPC_ADDR__: string
+declare var __MAIN_RPC_PORT__: string
+declare var __ROPSTEN_RPC_ADDR__: string
+declare var __ROPSTEN_RPC_PORT__: string
