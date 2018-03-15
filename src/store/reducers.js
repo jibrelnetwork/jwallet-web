73501d01b6924d962dfe2e4e364edb621f087467
Ivan Violentov <violentoff@gmail.com>
2018-03-15 23:06:50 +0300

    Move i18n & networks modules from deprecated JWallet route

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,44 +1,14 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 
-import currencies from 'routes/JWallet/modules/currencies'
-import keystore from 'routes/JWallet/modules/keystore'
-import networks from 'routes/JWallet/modules/networks'
-import transactions from 'routes/JWallet/modules/transactions'
-
-import alphaWarningModal from 'routes/JWallet/modules/modals/alphaWarning'
-import backupKeystoreModal from 'routes/JWallet/modules/modals/backupKeystore'
-import clearKeystoreModal from 'routes/JWallet/modules/modals/clearKeystore'
-import convertFundsModal from 'routes/JWallet/modules/modals/convertFunds'
-import customTokenModal from 'routes/JWallet/modules/modals/customToken'
-import importKeystoreAccountModal from 'routes/JWallet/modules/modals/importKeystoreAccount'
-import newDerivationPathModal from 'routes/JWallet/modules/modals/newDerivationPath'
-import newKeystoreAccountModal from 'routes/JWallet/modules/modals/newKeystoreAccount'
-import newKeystorePasswordModal from 'routes/JWallet/modules/modals/newKeystorePassword'
-import receiveFundsModal from 'routes/JWallet/modules/modals/receiveFunds'
-import sendFundsModal from 'routes/JWallet/modules/modals/sendFunds'
-
-import digitalAssets from 'routes/DigitalAssets/modules/digitalAssets'
+import networks from 'routes/modules/networks'
 import wallets from 'routes/Wallets/modules/wallets'
+import digitalAssets from 'routes/DigitalAssets/modules/digitalAssets'
 
 export const makeRootReducer = (asyncReducers: Object) => combineReducers({
-  currencies,
-  keystore,
   networks,
-  transactions,
-  alphaWarningModal,
-  backupKeystoreModal,
-  clearKeystoreModal,
-  convertFundsModal,
-  customTokenModal,
-  importKeystoreAccountModal,
-  newDerivationPathModal,
-  newKeystoreAccountModal,
-  newKeystorePasswordModal,
-  receiveFundsModal,
-  sendFundsModal,
-  digitalAssets,
   wallets,
+  digitalAssets,
   router,
   ...asyncReducers,
 })
--- src/utils/getPopularDigitalAssets.js
+++ src/utils/getPopularDigitalAssets.js
@@ -4,12 +4,12 @@ import { prepend } from 'ramda'
 
 import assets from 'data/assets'
 
-const networksNamesMap: { [string]: string } = {
+const networksNamesMap: { [?string]: ?string } = {
   '1': 'main',
 }
 
-const getPopularDigitalAssets = (networkId: NetworkId): DigitalAssets => {
-  const networkName: string = networksNamesMap[`${networkId}`]
+const getPopularDigitalAssets = (networkId: ?NetworkId): DigitalAssets => {
+  const networkName: ?string = networksNamesMap[networkId]
   const popularAssets: DigitalAssets = assets[networkName] || []
 
   return prepend(assets.ethereum)(popularAssets)
