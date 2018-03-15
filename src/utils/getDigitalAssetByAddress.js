c61e77e3c689d3da232ba598d15ece5780573318
Ivan Violentov <violentoff@gmail.com>
2018-03-15 15:24:56 +0300

    Update getDigitalAssetByAddress util method

--- src/utils/getDigitalAssetByAddress.js
+++ src/utils/getDigitalAssetByAddress.js
@@ -1,11 +1,21 @@
+// @flow
+
 import { compose, equals, filter, head, toLower } from 'ramda'
 
 const getDigitalAssetByAddress = (
-  assetAddress: Address,
+  assetAddress: ?Address,
   digitalAssets: DigitalAssets,
-): ?DigitalAsset => compose(
-  head,
-  filter(({ address }: DigitalAsset): boolean => equals(toLower(assetAddress), toLower(address))),
-)(digitalAssets)
+): ?DigitalAsset => {
+  if (!(assetAddress && digitalAssets.length)) {
+    return null
+  }
+
+  const lowerAssetAddress: Address = toLower(assetAddress)
+
+  return compose(
+    head,
+    filter(({ address }: DigitalAsset): boolean => equals(lowerAssetAddress, toLower(address))),
+  )(digitalAssets)
+}
 
 export default getDigitalAssetByAddress
