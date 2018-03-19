83b7808791c8bcb55e0559e08430a02461395dc8
Ivan Violentov <violentoff@gmail.com>
2018-03-19 18:44:18 +0300

    Add DigitalAssets component

---
+++ src/components/__new__/AssetCard/AssetCard.js
@@ -0,0 +1,54 @@
+// @flow
+
+import React from 'react'
+import classNames from 'classnames'
+
+import handle from 'utils/handle'
+import JIcon from 'components/base/__new__/JIcon'
+
+const AssetCard = ({
+  setActive,
+  address,
+  name,
+  symbol,
+  balance,
+  isActive,
+  isLoading,
+  color,
+}: Props) => {
+  const integer = Math.floor(balance)
+  const decimals = (balance - integer).toFixed(2).substr(1)
+
+  return (
+    <div
+      key={address}
+      onClick={handle(setActive)(address)}
+      className={classNames('asset-card', color && `-${color}`, { '-active': isActive })}
+    >
+      <JIcon size='large' name='token-ant' />
+      <div className='name'>{name}</div>
+      {(balance !== undefined) && (
+        <div className='balance'>
+          {isLoading ? 'Loading' : `${integer.toFixed()} ${decimals} ${symbol}`}
+        </div>
+      )}
+    </div>
+  )
+}
+
+type Props = {
+  setActive: Function,
+  address: Address,
+  name: string,
+  symbol: string,
+  balance: number,
+  isActive: boolean,
+  isLoading: boolean,
+  color?: 'blue' | 'white',
+}
+
+AssetCard.defaultProps = {
+  color: 'white',
+}
+
+export default AssetCard
