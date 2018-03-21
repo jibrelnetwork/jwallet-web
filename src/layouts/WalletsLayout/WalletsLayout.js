8112e3242d531794e17568d5aa169c5721cc08f4
Ivan Violentov <violentoff@gmail.com>
2018-03-21 13:42:03 +0300

    Update WalletsLayout

--- src/layouts/WalletsLayout/WalletsLayout.js
+++ src/layouts/WalletsLayout/WalletsLayout.js
@@ -3,16 +3,7 @@
 import React from 'react'
 
 const WalletsLayout = ({ children }: Props) => (
-  <div>
-    {
-    /*
-      <div>{'Aside'}</div>
-      <div>{'Title'}</div>
-      <div>{'ESC button'}</div>
-    */
-    }
-    <div>{children}</div>
-  </div>
+  <div className='wallets-layout'>{children}</div>
 )
 
 type Props = {
