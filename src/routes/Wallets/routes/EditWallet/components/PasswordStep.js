8882f0cd29afb6640303b045fab6cf5078c0f8c9
Ivan Violentov <violentoff@gmail.com>
2018-03-21 14:49:50 +0300

    Update EditWallet view

--- src/routes/Wallets/routes/EditWallet/components/PasswordStep.js
+++ src/routes/Wallets/routes/EditWallet/components/PasswordStep.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 
-import { JButton, JTextInput } from 'components/base'
+import { JButton, JInput } from 'components/base/__new__'
 
 const PasswordStep = ({
   setPassword,
@@ -10,17 +10,23 @@ const PasswordStep = ({
   invalidFields,
   password,
 }: Props) => (
-  <div className='edit-wallet-password-step'>
-    <JTextInput
-      onValueChange={setPassword}
+  <div className='form'>
+    <JInput
+      onChange={setPassword}
       value={password}
+      type='password'
       name='edit-wallet-password'
       errorMessage={invalidFields.password}
       placeholder={i18n('routes.editWallet.placeholder.password')}
-      editable
-      secureTextEntry
     />
-    <JButton onClick={setNextStep} label={i18n('routes.editWallet.buttonTitle.confirm')} blue />
+    <div className='actions'>
+      <JButton
+        onClick={setNextStep}
+        text={'routes.editWallet.buttonTitle.confirm'}
+        color='blue'
+        large
+      />
+    </div>
   </div>
 )
 
