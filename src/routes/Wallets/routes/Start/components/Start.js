33b9d97d464f8de1275925a232bff5a783befdc1
Ivan Violentov <violentoff@gmail.com>
2018-03-26 00:56:55 +0300

    Integrate WalletHeader

--- src/routes/Wallets/routes/Start/components/Start.js
+++ src/routes/Wallets/routes/Start/components/Start.js
@@ -3,17 +3,24 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import WalletHeader from 'components/__new__/WalletHeader'
+
 const Start = () => (
-  <div className='start-view'>
-    <div style={{ marginBottom: '40px' }}>
-      {'Create a new key pair or import anexisting one to get started'}
+  <div className='content'>
+    <div className='wallet-header-wrapper'>
+      <WalletHeader />
     </div>
-    <Link to='/wallets/create' style={{ display: 'block', marginBottom: '10px' }}>
-      {'Create new wallet'}
-    </Link>
-    <Link to='/wallets/import'>
-      {'Import wallet'}
-    </Link>
+    <div>
+      <div style={{ marginBottom: '40px' }}>
+        {'Create a new key pair or import anexisting one to get started'}
+      </div>
+      <Link to='/wallets/create' style={{ display: 'block', marginBottom: '10px' }}>
+        {'Create new wallet'}
+      </Link>
+      <Link to='/wallets/import'>
+        {'Import wallet'}
+      </Link>
+    </div>
   </div>
 )
 
