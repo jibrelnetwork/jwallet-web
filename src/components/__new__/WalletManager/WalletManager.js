506a5a1cda1b85b857344caf49134c822fa45fe0
Ivan Violentov <violentoff@gmail.com>
2018-03-26 14:26:37 +0300

    Update WalletManager

--- src/components/__new__/WalletManager/WalletManager.js
+++ src/components/__new__/WalletManager/WalletManager.js
@@ -4,146 +4,81 @@
 
 import React from 'react'
 
-import { JText, JIcon, JButton } from '../../base/__new__'
+import { JText, JIcon, JButton } from 'components/base/__new__'
+
+import Info from './WalletManagerInfo'
+import Actions from './WalletManagerActions'
+import Password from './WalletManagerPassword'
+
+const walletTypeIconMap = {
+  'address': 'private-key-read',
+  'privateKey': 'private-key',
+  'bip32Xpub': 'mnemonic-read',
+  'mnemonic': 'mnemonic',
+}
 
 const WalletManager = ({
-  mode,
-  icon,
-  text,
-  title,
-  setMode,
+  toggleWallet,
+  showActionsMenu,
+  setWalletAction,
+  setPassword,
+  setActive,
+  walletData,
+  invalidFields,
   password,
-  onEditClick,
-  onDeleteClick,
-  onBackupClick,
-  onPasswordChange,
-  onPasswordArrowClick,
-  onChangePasswordClick,
-}: Props) => (
-  <div className='WalletManager'>
-    {{
-      info: (
-        <div className='info'>
-          <div
-            className='content'
-            onClick={() => setMode('password')}
-          >
-            <div className='icon'>
-              <JIcon name={icon} size='medium' />
-            </div>
-            <div className='data'>
-              <div className='title'>
-                <JText
-                  value={title}
-                  variants={['white', 'large']}
-                />
-              </div>
-              <div className='text'>
-                <JText
-                  value={text}
-                  variants={['white', 'normal', 'transparent']}
-                />
-              </div>
-            </div>
-          </div>
-          <div className='actions'>
-            <JButton
-              iconName='dots-white'
-              iconSize='medium'
-              onClick={() => setMode('actions')}
-            />
-          </div>
-        </div>
-      ),
-      password: (
-        <div className='password'>
-          <div className='content'>
-            <div className='icon'>
-              <JIcon name={icon} size='medium' />
-            </div>
-            <div className='input'>
-              <input
-                type='password'
-                value={password}
-                onChange={onPasswordChange}
-                placeholder='Type your password'
-              />
-            </div>
-          </div>
-          <div className='arrow'>
-            <JButton
-              minimal
-              onClick={onPasswordArrowClick}
-              iconName='arrow'
-              iconSize='medium'
-            />
-          </div>
-        </div>
-      ),
-      actions: (
-        <div
-          className='actions'
-          onBlur={() => setMode('info')}
-        >
-          {onEditClick && (
-            <div className='edit'>
-              <JButton
-                text='Edit'
-                color='white'
-                minimal
-                onClick={onEditClick}
-              />
-            </div>
-          )}
-          {onBackupClick && (
-            <div className='backup'>
-              <JButton
-                text='Backup'
-                color='white'
-                minimal
-                onClick={onBackupClick}
-              />
-            </div>
-          )}
-          {onChangePasswordClick && (
-            <div className='change-password'>
-              <JButton
-                text='Change password'
-                color='white'
-                minimal
-                onClick={onChangePasswordClick}
-              />
-            </div>
-          )}
-          {onDeleteClick && (
-            <div className='delete'>
-              <JButton
-                text='Delete'
-                color='white'
-                minimal
-                onClick={onDeleteClick}
-              />
-            </div>
-          )}
-        </div>
-      ),
-    }[mode]}
-  </div>
-)
+  toggledWalletId,
+  showActionsWalletId,
+}: Props) => {
+  const { id, customType, isReadOnly }: Wallet = walletData
+  const icon = walletTypeIconMap[customType]
+  const isToggled: boolean = (id === toggledWalletId)
+  const isActionsMenuShown: boolean = (id === showActionsWalletId)
+
+  if (isToggled && !isReadOnly) {
+    return (
+      <div className='WalletManager'>
+        <Password
+          setPassword={setPassword}
+          setActive={setActive}
+          invalidFields={invalidFields}
+          password={password}
+          icon={icon}
+        />
+      </div>
+    )
+  }
+
+  if (isActionsMenuShown) {
+    return (
+      <div className='WalletManager'>
+        <Actions setWalletAction={setWalletAction} isReadOnly={isReadOnly} />
+      </div>
+    )
+  }
+
+  return (
+    <div className='WalletManager'>
+      <Info
+        toggleWallet={toggleWallet}
+        showActionsMenu={showActionsMenu}
+        walletData={walletData}
+        icon={icon}
+      />
+    </div>
+  )
+}
 
 type Props = {
-  icon: string,
-  text: string,
-  mode: 'info' | 'password' | 'actions',
-  title: string,
-  password: string,
-  setMode: (mode: 'info' | 'password' | 'actions') => void,
-  onEditClick: () => void,
-  onBackupClick: () => void,
-  onDeleteClick: () => void,
-  onPasswordChange: (event: Event) => void, // TODO: Remove?
-  onPasswordArrowClick: (password: string) => void,
-  onChangePasswordClick: () => void, // TODO: Rename
+  toggleWallet: (walletId: WalletId) => Dispatch,
+  showActionsMenu: (walletId: WalletId) => Dispatch,
+  setWalletAction: (walletAction: WalletAction) => Dispatch,
+  setPassword: (password: Password) => Dispatch,
+  setActive: () => Dispatch,
+  walletData: Wallet,
+  invalidFields: Object,
+  password: Password,
+  toggledWalletId: ?WalletId,
+  showActionsWalletId: ?WalletId,
 }
 
 export default WalletManager
