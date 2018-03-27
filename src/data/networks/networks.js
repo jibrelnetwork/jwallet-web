86e5c6c1c904eb71f2f4e2d1cde86ee47533a140
Ivan Violentov <violentoff@gmail.com>
2018-03-27 12:54:53 +0300

    Update main node endpoint

--- src/data/networks/networks.js
+++ src/data/networks/networks.js
@@ -5,8 +5,8 @@ const isValid = (v: string): boolean => !/\[|\]/.test(v)
 const networks: Networks = [{
   id: '1',
   title: 'main',
-  rpcaddr: isValid(__MAIN_RPC_ADDR__) ? __MAIN_RPC_ADDR__ : 'ns3089219.ip-37-59-55.eu',
-  rpcport: isValid(__MAIN_RPC_PORT__) ? __MAIN_RPC_PORT__ : '10001',
+  rpcaddr: isValid(__MAIN_RPC_ADDR__) ? __MAIN_RPC_ADDR__ : 'main-node.jwallet.network',
+  rpcport: isValid(__MAIN_RPC_PORT__) ? __MAIN_RPC_PORT__ : '443',
   ssl: true,
   isCustom: false,
 }, {
