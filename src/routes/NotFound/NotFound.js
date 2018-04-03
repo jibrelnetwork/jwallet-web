58e37bad410de43b42e93ac280a19085b6b34a5a
erohin <erohin.em@gmail.com>
2018-04-03 15:51:54 +0300

    Add component for 404 page #126

---
+++ src/routes/NotFound/NotFound.js
@@ -0,0 +1,34 @@
+// TODO: Replace button styles, add title and description
+
+/* @flow */
+
+import React from 'react'
+
+import { WalletHeader } from '../../components/__new__'
+import { JButton, JThumbnail } from '../../components/base/__new__'
+
+const NotFound = () => (
+  <div className='NotFound'>
+    <div className='header'>
+      <WalletHeader />
+    </div>
+    <div className='content'>
+      <JThumbnail
+        color='white'
+        image='man-blue'
+        title='404. Page not found'
+        description={`All user data, including imported or generated 
+private keys are stored locally, meaning your private`}
+      />
+    </div>
+    <div className='button'>
+      <JButton
+        text='Go to the front'
+        color='blue'
+        large
+      />
+    </div>
+  </div>
+)
+
+export default NotFound
