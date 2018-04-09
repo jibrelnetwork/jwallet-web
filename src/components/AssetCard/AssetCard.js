22c1b68928e14868371d9c6f369be4efaa3e3cb4
Ivan Violentov <violentoff@gmail.com>
2018-04-09 23:05:09 +0300

    Update AssetCard

--- src/components/AssetCard/AssetCard.js
+++ src/components/AssetCard/AssetCard.js
@@ -2,34 +2,51 @@
 
 import React from 'react'
 import classNames from 'classnames'
+import { identity } from 'ramda'
 
-import JIcon from 'components/base/JIcon'
-import handle from 'utils/eventHandlers/handle'
+import ethereum from 'data/assets/ethereum'
+import { JButton, JIcon } from 'components/base'
+import { handle, ignoreEvent } from 'utils/eventHandlers'
 
 const AssetCard = ({
+  edit,
   setActive,
   address,
   name,
   symbol,
   balance,
+  isCustom,
   isActive,
   isLoading,
   color,
 }: Props) => {
-  const integer = Math.floor(balance)
-  const decimals = (balance - integer).toFixed(2).substr(1)
+  const integer = balance ? Math.floor(balance) : 0
+  const decimals = balance ? (balance - integer).toFixed(2).substr(1) : 0
 
   return (
     <div
-      key={address}
       onClick={handle(setActive)(address)}
       className={classNames('asset-card', color && `-${color}`, { '-active': isActive })}
     >
-      <JIcon size='large' name='token-ant' />
+      <div className='icon'>
+        <JIcon size='large' name='token-ant' color={color} />
+      </div>
       <div className='name'>{name}</div>
       {(balance !== undefined) && (
         <div className='balance'>
           {isLoading ? 'Loading' : `${integer.toFixed()} ${decimals} ${symbol}`}
+        </div>
+      )}
+      {isCustom && (
+        <div className='edit'>
+          <JButton
+            onClick={ignoreEvent(edit)(address)}
+            iconName='settings'
+            iconSize='small'
+            color='gray'
+            minimal
+            transparent
+          />
         </div>
       )}
     </div>
@@ -37,18 +54,29 @@ const AssetCard = ({
 }
 
 type Props = {
+  edit: Function,
   setActive: Function,
   address: Address,
   name: string,
   symbol: string,
-  balance: number,
+  color: 'blue' | 'white',
+  balance: ?number,
+  isCustom: boolean,
   isActive: boolean,
   isLoading: boolean,
-  color?: 'blue' | 'white',
 }
 
 AssetCard.defaultProps = {
+  edit: identity,
+  setActive: identity,
+  address: ethereum.address,
+  name: ethereum.name,
+  symbol: ethereum.symbol,
   color: 'white',
+  balance: undefined,
+  isCustom: false,
+  isActive: false,
+  isLoading: false,
 }
 
 export default AssetCard
