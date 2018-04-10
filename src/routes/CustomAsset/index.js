9301f5ca8109ec7876ee9c105fe2a3544abdbe3a
Ivan Violentov <violentoff@gmail.com>
2018-04-10 18:32:53 +0300

    Update redirect from /custom-asset/

--- src/routes/CustomAsset/index.js
+++ src/routes/CustomAsset/index.js
@@ -6,7 +6,7 @@ import Edit from './routes/Edit'
 export default store => ({
   path: 'custom-asset',
   component: ModalLayout,
-  indexRoute: { onEnter: (nextState, replace) => replace('/') },
+  indexRoute: { onEnter: (nextState, replace) => replace('/custom-asset/add') },
   childRoutes: [
     Add(store),
     Edit(store),
