a029b650928bc25dcf8b6274c7fc2974c54547cc
Ivan Violentov <violentoff@gmail.com>
2018-04-13 18:09:14 +0300

    Update WalletsLayout

--- src/layouts/WalletsLayout/WalletsLayout.js
+++ src/layouts/WalletsLayout/WalletsLayout.js
@@ -1,9 +1,14 @@
 // @flow
 
 import React from 'react'
+import { Scrollbars } from 'react-custom-scrollbars'
 
 const WalletsLayout = ({ children }: Props) => (
-  <div className='wallets-layout'>{children}</div>
+  <div className='wallets-layout'>
+    <Scrollbars autoHide>
+      {children}
+    </Scrollbars>
+  </div>
 )
 
 type Props = {
