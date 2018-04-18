78e7a8c92c340451cdb357f7f9e932ca5a9a2a78
Ivan Violentov <violentoff@gmail.com>
2018-04-18 15:29:42 +0300

    Add balanceToString util method

--- src/utils/digitalAssets/index.js
+++ src/utils/digitalAssets/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+import balanceToString from './balanceToString'
 import filterDigitalAssets from './filterDigitalAssets'
 import getActiveDigitalAssetsData from './getActiveDigitalAssetsData'
 import getAssetDecimals from './getAssetDecimals'
@@ -11,6 +12,7 @@ import isJNT from './isJNT'
 import searchDigitalAssets from './searchDigitalAssets'
 
 export {
+  balanceToString,
   filterDigitalAssets,
   getActiveDigitalAssetsData,
   getAssetDecimals,
