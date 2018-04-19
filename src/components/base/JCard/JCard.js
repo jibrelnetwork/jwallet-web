4e5577f88deecb83930ea182ebfb16dbf9115816
Ivan Violentov <violentoff@gmail.com>
2018-04-19 14:46:35 +0300

    Update JCard

--- src/components/base/JCard/JCard.js
+++ src/components/base/JCard/JCard.js
@@ -3,15 +3,31 @@
 import React from 'react'
 import classNames from 'classnames'
 
-const JCard = ({ withShadow, children }: Props) => (
+const JCard = ({ children, title, withShadow }: Props) => (
   <div className={classNames('j-card', { '-with-shadow': withShadow })}>
-    {children}
+    {title && (
+      <div className='title'>
+        {title}
+      </div>
+    )}
+    {children && (
+      <div className='content'>
+        {children}
+      </div>
+    )}
   </div>
 )
 
 type Props = {
-  children: Object,
+  children: ?Object,
+  title: ?string,
   withShadow: ?bool,
+}
+
+JCard.defaultProps = {
+  children: null,
+  title: null,
+  withShadow: false,
 }
 
 export default JCard
