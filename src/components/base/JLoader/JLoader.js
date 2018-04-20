b598ce04fc917848ae7ed6ac2435d24045653c38
Ivan Violentov <violentoff@gmail.com>
2018-04-20 18:53:30 +0300

    Update JLoader (add color prop)

--- src/components/base/JLoader/JLoader.js
+++ src/components/base/JLoader/JLoader.js
@@ -2,12 +2,20 @@
 
 import React from 'react'
 
-const JLoader = () => (
-  <div className='j-loader'>
+const JLoader = ({ color }: Props) => (
+  <div className={`j-loader -${color}`}>
     <div className='dot -first' />
     <div className='dot -second' />
     <div className='dot -third' />
   </div>
 )
+
+type Props = {
+  color: 'blue' | 'gray' | 'white',
+}
+
+JLoader.defaultProps = {
+  color: 'white',
+}
 
 export default JLoader
