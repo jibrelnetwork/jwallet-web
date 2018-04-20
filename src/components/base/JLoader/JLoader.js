a6d31157f62463bbf865156eeeeeb5790f72a346
Ivan Violentov <violentoff@gmail.com>
2018-04-20 12:31:11 +0300

    Update JLoader

--- src/components/base/JLoader/JLoader.js
+++ src/components/base/JLoader/JLoader.js
@@ -1,18 +1,13 @@
+// @flow
+
 import React from 'react'
-import PropTypes from 'prop-types'
 
-function JLoader({ fixed }) {
-  const className = `loader ${fixed ? 'loader--fixed' : ''}`
-
-  return <div className={className} />
-}
-
-JLoader.propTypes = {
-  fixed: PropTypes.bool,
-}
-
-JLoader.defaultProps = {
-  fixed: false,
-}
+const JLoader = () => (
+  <div className='j-loader'>
+    <div className='dot -first' />
+    <div className='dot -second' />
+    <div className='dot -third' />
+  </div>
+)
 
 export default JLoader
