81c5af63cfe6e9fc593b8a16a38a3f35780e9a9e
Ivan Violentov <violentoff@gmail.com>
2018-04-28 15:30:29 +0300

    Add JText.stories

--- stories/components/base/index.js
+++ stories/components/base/index.js
@@ -1,3 +1,4 @@
 // @flow
 
 import './JIcon.stories'
+import './JText.stories'
--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,13 @@
 // @flow
 
+import i18n from '../src/i18n'
+
+if (typeof window !== 'undefined') {
+  window.i18n = i18n()
+}
+
 import '../src/styles/core.scss'
+import './styles.css'
 
 import './components'
 import './components/base'
