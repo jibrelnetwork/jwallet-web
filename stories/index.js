fe50970f015692f755dca716a35d72c4af01daac
Dragonesis <dragonesis@ya.ru>
2018-10-01 16:00:06 +0300

    JWWEB-47 Update JRaisedButton component (#327)
    * JWWEB-47 Update JRaisedButton component
    
    * Update in accordance with the requirements of reviewers

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,12 @@
 // @flow
 
-import i18n from '../src/i18n'
-
 import '../src/styles/core.scss'
 import './styles.css'
-import './__new__'
+
+import './components'
+import './components/base'
+
+import i18n from '../src/i18n'
 
 if (typeof window !== 'undefined') {
   window.i18n = i18n()
