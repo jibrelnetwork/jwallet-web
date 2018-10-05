51cbf2b295dcc80ed1c7609936dccd6b4d880aa4
Ivan Violentov <violentoff@gmail.com>
2018-10-05 22:36:14 +0300

    Update WalletDataStep component (props names)

--- src/components/WalletDataStep/WalletDataStep.js
+++ src/components/WalletDataStep/WalletDataStep.js
@@ -11,9 +11,9 @@ type Props = {|
   +onChangePassphrase: Function,
   +onChangeDerivationPath: Function,
   +invalidFields: FormFields,
-  +data: string,
-  +passphrase: string,
-  +derivationPath: string,
+  +valueData: string,
+  +valuePassphrase: string,
+  +valueDerivationPath: string,
   +isLoading: boolean,
   +isMnemonic: boolean,
 |}
@@ -24,16 +24,16 @@ const WalletDataStep = ({
   onChangePassphrase,
   onChangeDerivationPath,
   invalidFields,
-  data,
-  passphrase,
-  derivationPath,
+  valueData,
+  valuePassphrase,
+  valueDerivationPath,
   isLoading,
   isMnemonic,
 }: Props) => (
   <form className='wallet-data-step' onSubmit={ignoreEvent(onSubmit)()}>
     <JInput
       onChange={onChangeData}
-      value={data}
+      value={valueData}
       errorMessage={invalidFields.data}
       type='text'
       color='white'
@@ -44,7 +44,7 @@ const WalletDataStep = ({
       <JInput
         key='passphrase'
         onChange={onChangePassphrase}
-        value={passphrase}
+        value={valuePassphrase}
         errorMessage={invalidFields.passphrase}
         color='white'
         name='wallet-passphrase'
@@ -53,7 +53,7 @@ const WalletDataStep = ({
       <JInput
         key='derivation-path'
         onChange={onChangeDerivationPath}
-        value={derivationPath}
+        value={valueDerivationPath}
         errorMessage={invalidFields.derivationPath}
         color='white'
         name='wallet-derivation-path'
