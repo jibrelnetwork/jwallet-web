542f6135a6fe9bc3d6bb7192893e09209a111770
Ivan Violentov <violentoff@gmail.com>
2018-10-09 14:12:59 +0300

    Update wallets worker

--- src/workers/wallets/worker.js
+++ src/workers/wallets/worker.js
@@ -4,44 +4,72 @@ import utils from 'jwallet-web-keystore'
 
 import keystore from 'services/keystore'
 
-import {
-  CREATE_REQUEST,
-  CHECK_NAME_REQUEST,
-  createError,
-  createSuccess,
-  checkNameError,
-  checkNameSuccess,
-} from 'routes/Wallets/routes/Create/modules/walletsCreate'
+import * as walletsCreate from 'routes/Wallets/routes/Create/modules/walletsCreate'
+import * as walletsImport from 'routes/Wallets/routes/Import/modules/walletsImport'
 
 import type { WalletsCreateAction } from 'routes/Wallets/routes/Create/modules/walletsCreate'
+import type { WalletsImportAction } from 'routes/Wallets/routes/Import/modules/walletsImport'
+
+type WalletsAction = WalletsCreateAction | WalletsImportAction
 
 export type WalletsWorkerMessage = {|
-  +data: WalletsCreateAction,
+  +data: WalletsAction,
 |}
 
 // eslint-disable-next-line fp/no-mutation
 onmessage = (msg: WalletsWorkerMessage): void => {
-  const action: WalletsCreateAction = msg.data
+  const action: WalletsAction = msg.data
 
   switch (action.type) {
-    case CHECK_NAME_REQUEST: {
+    case walletsCreate.CHECK_NAME_REQUEST: {
       try {
         const { wallets, name } = action.payload
 
         keystore.checkWalletUniqueness(wallets, name, 'name')
 
-        postMessage(checkNameSuccess())
+        postMessage(walletsCreate.checkNameSuccess())
       } catch (err) {
         // eslint-disable-next-line no-console
         console.error(err)
 
-        postMessage(checkNameError(err.message))
+        postMessage(walletsCreate.checkNameError(err.message))
       }
 
       break
     }
 
-    case CREATE_REQUEST: {
+    case walletsImport.CHECK_WALLET_TYPE_REQUEST: {
+      const { data } = action.payload
+
+      if (utils.checkMnemonicValid(data)) {
+        postMessage(walletsImport.checkWalletTypeSuccess('mnemonic'))
+      } else if (utils.checkBip32XPublicKeyValid(data)) {
+        postMessage(walletsImport.checkWalletTypeSuccess('bip32Xpub'))
+      } else if (utils.checkPrivateKeyValid(data)) {
+        postMessage(walletsImport.checkWalletTypeSuccess('privateKey'))
+      } else if (utils.checkAddressValid(data)) {
+        postMessage(walletsImport.checkWalletTypeSuccess('address'))
+      } else {
+        postMessage(walletsImport.checkWalletTypeError('Please input valid wallet data'))
+      }
+
+      break
+    }
+
+    case walletsImport.CHECK_DERIVATION_PATH_REQUEST: {
+      const { derivationPath } = action.payload
+      const isValid: boolean = utils.checkDerivationPathValid(derivationPath)
+
+      if (isValid) {
+        postMessage(walletsImport.checkDerivationPathSuccess())
+      } else {
+        postMessage(walletsImport.checkDerivationPathError('Derivation path is not valid'))
+      }
+
+      break
+    }
+
+    case walletsCreate.CREATE_REQUEST: {
       try {
         const {
           wallets,
@@ -63,7 +91,7 @@ onmessage = (msg: WalletsWorkerMessage): void => {
           keystore.checkPassword(testPasswordData, password, passwordOpts)
         }
 
-        postMessage(createSuccess({
+        postMessage(walletsCreate.createSuccess({
           passwordHint,
           passwordOptions: passwordOpts,
           mnemonicOptions: mnemonicOpts,
@@ -79,7 +107,52 @@ onmessage = (msg: WalletsWorkerMessage): void => {
         // eslint-disable-next-line no-console
         console.error(err)
 
-        postMessage(createError(err.message))
+        postMessage(walletsCreate.createError(err.message))
+      }
+
+      break
+    }
+
+    case walletsImport.IMPORT_REQUEST: {
+      try {
+        const {
+          wallets,
+          passwordOptions,
+          mnemonicOptions,
+          testPasswordData,
+          data,
+          name,
+          password,
+          passwordHint,
+        } = action.payload
+
+        // eslint-disable-next-line fp/no-mutation, no-restricted-globals
+        self.window = self
+
+        const passwordOpts: PasswordOptions = utils.getPasswordOptions(passwordOptions)
+        const mnemonicOpts: MnemonicOptions = utils.getMnemonicOptions(mnemonicOptions)
+
+        if (testPasswordData) {
+          keystore.checkPassword(testPasswordData, password, passwordOpts)
+        }
+
+        postMessage(walletsImport.importSuccess({
+          passwordHint,
+          passwordOptions: passwordOpts,
+          mnemonicOptions: mnemonicOpts,
+          testPasswordData: testPasswordData || keystore.initPassword(password, passwordOpts),
+          items: keystore.createWallet(wallets, {
+            data,
+            name,
+            passwordOptions: passwordOpts,
+            mnemonicOptions: mnemonicOpts,
+          }, password),
+        }))
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err)
+
+        postMessage(walletsImport.importError(err.message))
       }
 
       break
--- src/workers/wallets/wrapper.js
+++ src/workers/wallets/wrapper.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as walletsCreate from 'routes/Wallets/routes/Create/modules/walletsCreate'
+import * as walletsImport from 'routes/Wallets/routes/Import/modules/walletsImport'
 
 import type { WalletsCreateAction } from 'routes/Wallets/routes/Create/modules/walletsCreate'
 
@@ -14,19 +15,34 @@ type WalletsWorkerInstance = {|
   +postMessage: (WalletsCreateAction) => void,
 |}
 
+type ImportWalletData = {|
+  +data: string,
+  +name: string,
+  +passphrase: string,
+  +derivationPath: string,
+|}
+
 // $FlowFixMe
 const walletsWorker: WalletsWorkerInstance = new WalletsWorker()
 
-export const checkNameRequest = (wallets: Wallets, name: string) => {
+export function checkNameRequest(wallets: Wallets, name: string) {
   walletsWorker.postMessage(walletsCreate.checkNameRequest(wallets, name))
 }
 
-export const createRequest = (
+export function checkWalletTypeRequest(data: string) {
+  walletsWorker.postMessage(walletsImport.checkWalletTypeRequest(data))
+}
+
+export function checkDerivationPathRequest(derivationPath: string) {
+  walletsWorker.postMessage(walletsImport.checkDerivationPathRequest(derivationPath))
+}
+
+export function createRequest(
   walletsData: WalletsState,
   name: string,
   password: string,
   passwordHint: ?string,
-) => {
+) {
   const {
     items,
     passwordOptions,
@@ -45,7 +61,45 @@ export const createRequest = (
   }))
 }
 
-export const run = (store: { dispatch: (WalletsCreateAction) => void }) => {
+export function importRequest(
+  walletsData: WalletsState,
+  importWalletData: ImportWalletData,
+  password: string,
+  passwordHint: ?string,
+) {
+  const {
+    items,
+    passwordOptions,
+    mnemonicOptions,
+    testPasswordData,
+  }: WalletsState = walletsData
+
+  const {
+    data,
+    name,
+    passphrase,
+    derivationPath,
+  }: ImportWalletData = importWalletData
+
+  const mnemonicOptionsUser: MnemonicOptionsUser = {
+    ...mnemonicOptions,
+    passphrase,
+    derivationPath,
+  }
+
+  walletsWorker.postMessage(walletsImport.importRequest({
+    data,
+    name,
+    password,
+    passwordOptions,
+    testPasswordData,
+    wallets: items,
+    mnemonicOptions: mnemonicOptionsUser,
+    passwordHint: passwordHint || walletsData.passwordHint,
+  }))
+}
+
+export function run(store: { dispatch: (WalletsCreateAction) => void }) {
   walletsWorker.onmessage = function walletsWorkerOnMessage(msg: WalletsWorkerMessage) {
     store.dispatch(msg.data)
   }
