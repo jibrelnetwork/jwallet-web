9bcde53625424441111597a61c106ef1e99a14ef
Michael Maslov <flexxnn@gmail.com>
2018-10-09 20:04:33 +0300

    JRaisedButton, default props, pure component

--- src/components/base/JRaisedButton/JRaisedButton.js
+++ src/components/base/JRaisedButton/JRaisedButton.js
@@ -1,75 +1,106 @@
 // @flow
 
-import React from 'react'
-import classNames from 'classnames'
+import React, { PureComponent } from 'react'
+import cx from 'classnames'
 import handle from 'utils/eventHandlers/handle'
 
 import { JIcon, JText, JLoader } from 'components/base'
 
-const JRaisedButton = ({
-  onClick,
-  onHover,
-  label,
-  color,
-  iconName,
-  iconSize,
-  iconColor,
-  labelColor,
-  loaderColor,
-  isWide,
-  isDisabled,
-  isLoading,
-  isHovered,
-}: Props) => {
-  const buttonClassName = classNames(
-    `j-raised-button -${color}`,
-    isWide && '-wide',
-    isDisabled && '-disabled',
-  )
+type Props = {
+  onClick: ?Function,
+  label: string,
+  iconName: ?string,
+  color: 'blue' | 'white',
+  iconColor: 'blue' | 'white',
+  iconSize: 'small' | 'medium',
+  labelColor: 'blue' | 'white',
+  loaderColor: 'blue' | 'white',
+  isWide: boolean,
+  isDisabled: boolean,
+  isLoading: boolean,
+}
 
-  const labelColorHovered = labelColor === 'blue' ? 'white' : 'white'
+type ButtonState = {
+  isHovered: boolean,
+}
 
-  if (isLoading) {
+class JRaisedButton extends PureComponent<Props, ButtonState> {
+  static defaultProps = {
+    iconName: null,
+    color: 'blue',
+    iconColor: 'white',
+    iconSize: 'medium',
+    labelColor: 'white',
+    loaderColor: 'white',
+    isWide: false,
+    isDisabled: false,
+    isLoading: false,
+    isHovered: false,
+  }
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isHovered: false,
+    }
+  }
+
+  onHover = (isHovered: boolean) => {
+    this.setState({ isHovered })
+  }
+
+  render() {
+    const {
+      onClick,
+      label,
+      color,
+      iconName,
+      iconSize,
+      iconColor,
+      labelColor,
+      loaderColor,
+      isWide,
+      isDisabled,
+      isLoading,
+    } = this.props
+
+    const {
+      isHovered,
+    } = this.state
+
+    const buttonClassName = cx(
+      `j-raised-button -${color}`,
+      isWide && '-wide',
+      isDisabled && '-disabled',
+    )
+
+    if (isLoading) {
+      return (
+        <div className={`${buttonClassName} -loading`}>
+          <JLoader color={loaderColor} />
+        </div>
+      )
+    }
+
     return (
-      <div className={`${buttonClassName} -loading`}>
-        <JLoader color={loaderColor} />
+      <div
+        onClick={isDisabled ? undefined : onClick}
+        onMouseEnter={handle(this.onHover)(true)}
+        onMouseLeave={handle(this.onHover)(false)}
+        className={buttonClassName}
+      >
+        {iconName && (
+          <div className='icon'>
+            <JIcon name={iconName} size={iconSize} color={iconColor} />
+          </div>
+        )}
+        <div className='label'>
+          <JText value={label} color={isHovered ? 'white' : labelColor} weight='bold' />
+        </div>
       </div>
     )
   }
-
-  return (
-    <div
-      onClick={isDisabled ? null : onClick}
-      onMouseEnter={handle(onHover)(true)}
-      onMouseLeave={handle(onHover)(false)}
-      className={buttonClassName}
-    >
-      {iconName && (
-        <div className='icon'>
-          <JIcon name={iconName} size={iconSize} color={iconColor} />
-        </div>
-      )}
-      <div className='label'>
-        <JText value={label} color={isHovered ? labelColorHovered : labelColor} weight='bold' />
-      </div>
-    </div>
-  )
-}
-
-type Props = {
- onClick: Function,
- onHover: Function,
- label: string,
- iconName: ?string,
- color: 'blue' | 'white',
- iconColor: 'blue' | 'white',
- iconSize: 'small' | 'medium',
- labelColor: 'blue' | 'white',
- loaderColor: 'blue' | 'white',
- isWide: boolean,
- isDisabled: boolean,
- isLoading: boolean,
- isHovered: boolean,
 }
 
 export default JRaisedButton
