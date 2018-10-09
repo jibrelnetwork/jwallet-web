8973656c44bb34b2a05173a0ef7a1810e066af9b
Ivan Violentov <violentoff@gmail.com>
2018-10-09 14:11:34 +0300

    Update router config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,30 @@
+// @flow
+
 import CoreLayout from 'layouts/CoreLayout'
 
+/*
 import DigitalAssets from './DigitalAssets'
 import CustomAsset from './CustomAsset'
 import Funds from './Funds'
 import Transactions from './Transactions'
+*/
 import Wallets from './Wallets'
 import NotFound from './NotFound'
 
-const createRoutes = store => ({
+const createRoutes = () => ({
   path: '/',
   component: CoreLayout,
-  indexRoute: { onEnter: (nextState, replace) => replace('/transactions') },
+  indexRoute: {
+    onEnter: (nextState: State, replace: (string) => void) => replace('/transactions'),
+  },
   childRoutes: [
+    /*
     DigitalAssets(store),
     CustomAsset(store),
     Funds(store),
     Transactions(store),
-    Wallets(store),
+    */
+    Wallets,
     NotFound(),
   ],
 })
