27f95c62df2c5605bfc9b63e7ec69fe071064f22
Ivan Violentov <violentoff@gmail.com>
2018-10-10 19:32:40 +0300

    Update wallets routes config

--- src/routes/Wallets/index.js
+++ src/routes/Wallets/index.js
@@ -5,10 +5,10 @@ import WalletsLayout from 'layouts/WalletsLayout'
 import Start from './routes/Start'
 import Create from './routes/Create'
 import Import from './routes/Import'
-import View from './WalletsIndexViewContainer'
+import Rename from './routes/Rename'
+import WalletsIndex from './WalletsIndexViewContainer'
 /*
 import Addresses from './routes/Addresses'
-import Edit from './routes/Edit'
 import Backup from './routes/Backup'
 import ChangePassword from './routes/ChangePassword'
 import Remove from './routes/Remove'
@@ -18,15 +18,15 @@ export default {
   path: 'wallets',
   component: WalletsLayout,
   indexRoute: {
-    component: View,
+    component: WalletsIndex,
   },
   childRoutes: [
     Start,
     Create,
     Import,
+    Rename,
     /*
     Addresses(store),
-    Edit(store),
     Backup(store),
     ChangePassword(store),
     Remove(store),
