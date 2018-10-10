6b65f79184862e389c890dd54d4c61b8a5b7aff8
Michael Maslov <flexxnn@gmail.com>
2018-10-10 17:38:41 +0300

    change routes

--- src/routes/CustomAsset/index.js
+++ src/routes/CustomAsset/index.js
@@ -1,15 +1,15 @@
 import ModalLayout from 'layouts/ModalLayout'
 
 import Add from './routes/Add'
-import Edit from './routes/Edit'
 
-export default store => ({
+export default () => ({
   path: 'custom-asset',
   component: ModalLayout,
-  indexRoute: { onEnter: (nextState, replace) => replace('/custom-asset/add') },
   childRoutes: [
-    Add(store),
-    Edit(store),
+    Add(),
   ],
+  indexRoute: {
+    onEnter: (nextState, replace) => replace('/custom-asset/add'),
+  },
 })
 
--- src/routes/CustomAsset/routes/Add/index.js
+++ src/routes/CustomAsset/routes/Add/index.js
@@ -1,13 +1,6 @@
-import { injectReducer } from 'store/reducers'
+import { CustomAssetAddContainer } from '../../containers'
 
-export default store => ({
+export default AddRoute = () => ({
   path: 'add',
-  getComponent(nextState, cb) {
-    require.ensure([], (require) => {
-      const CustomAsset = require('../../containers/CustomAssetContainer').default
-      const customAsset = require('../../modules/customAsset').default
-      injectReducer(store, { key: 'customAsset', reducer: customAsset })
-      cb(null, CustomAsset)
-    }, 'custom-asset-add')
-  },
+  component: CustomAssetAddContainer,
 })
