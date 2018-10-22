4ed030ab19ef29991998840c9df0068e7ed967d2
Ivan Violentov <violentoff@gmail.com>
2018-10-22 17:52:29 +0300

    Update WalletFace component

--- src/components/WalletFace/WalletFace.js
+++ src/components/WalletFace/WalletFace.js
@@ -1,67 +1,73 @@
 // @flow
 
-import React from 'react'
+import React, { PureComponent } from 'react'
 import classNames from 'classnames'
 
 import { JCard, JFlatButton, JIcon, JText } from 'components/base'
 
-const WalletFace = ({
-  onClick,
-  showActions,
-  title,
-  iconName,
-  description,
-  isTransparent,
-  isReadOnly,
-}: Props) => (
-  <JCard color='blue'>
-    <div onClick={onClick} className='wallet-face'>
-      <div className={classNames('icon', (isTransparent || isReadOnly) && '-transparent')}>
-        <JIcon name={iconName} size='medium' color='white' />
-      </div>
-      <div className='data'>
-        <div className='title'>
-          <JText value={title} size='large' color='white' />
-        </div>
-        <div className='description'>
-          {isReadOnly && (
-            <div className='icon'><JIcon name='eye' size='medium' color='white' /></div>
-          )}
-          <JText value={description} color='white' />
-        </div>
-      </div>
-      {showActions && (
-        <div className='actions'>
-          <div className='button'>
-            <JFlatButton
-              onClick={showActions}
-              iconName='dots'
-              iconSize='medium'
-              iconColor='white'
-              isTransparent
-              isHoverOpacity
-            />
+type Props = {|
+  +onClick: Function,
+  +showActions: ?Function,
+  +title: string,
+  +iconName: string,
+  +description: string,
+  +isReadOnly: boolean,
+  +isTransparent: boolean,
+|}
+
+class WalletFace extends PureComponent<Props> {
+  static defaultProps = {
+    showActions: null,
+    isReadOnly: false,
+    isTransparent: false,
+  }
+
+  render() {
+    const {
+      onClick,
+      showActions,
+      title,
+      iconName,
+      description,
+      isReadOnly,
+      isTransparent,
+    }: Props = this.props
+
+    return (
+      <JCard color='blue'>
+        <div onClick={onClick} className='wallet-face'>
+          <div className={classNames('icon', (isTransparent || isReadOnly) && '-transparent')}>
+            <JIcon name={iconName} size='medium' color='white' />
           </div>
+          <div className='data'>
+            <div className='title'>
+              <JText value={title} size='large' color='white' />
+            </div>
+            <div className='description'>
+              {isReadOnly && (
+                <div className='icon'><JIcon name='eye' size='medium' color='white' /></div>
+              )}
+              <JText value={description} color='white' />
+            </div>
+          </div>
+          {showActions && (
+            <div className='actions'>
+              <div className='button'>
+                <JFlatButton
+                  onClick={showActions}
+                  iconName='dots'
+                  iconSize='medium'
+                  iconColor='white'
+                  isTransparent
+                  isHoverOpacity
+                />
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  </JCard>
-)
-
-type Props = {
-  onClick: Function,
-  showActions: ?Function,
-  title: string,
-  iconName: string,
-  description: string,
-  isTransparent: boolean,
-  isEyeIcon: boolean,
-}
-
-WalletFace.defaultProps = {
-  showActions: null,
-  isTransparent: false,
-  isEyeIcon: false,
+      </JCard>
+    )
+  }
 }
 
 export default WalletFace
