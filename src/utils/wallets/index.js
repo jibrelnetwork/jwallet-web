1ec1b8950e9b5847154f010ffe007b7262958043
Ivan Violentov <violentoff@gmail.com>
2018-10-26 12:43:29 +0300

    Add wallets/getShortenedAddress function

--- src/utils/wallets/index.js
+++ src/utils/wallets/index.js
@@ -2,8 +2,10 @@
 
 import getWallet from './getWallet'
 import checkMnemonicType from './checkMnemonicType'
+import getShortenedAddress from './getShortenedAddress'
 
 export {
   getWallet,
   checkMnemonicType,
+  getShortenedAddress,
 }
