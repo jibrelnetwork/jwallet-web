88b981599af58ca1a7286bcef7cabef9d444813f
Ivan Violentov <ivan.violentov@jibrel.network>
2018-10-27 19:09:51 +0300

    Update MnemonicAddresses

--- src/components/MnemonicAddresses/MnemonicAddresses.js
+++ src/components/MnemonicAddresses/MnemonicAddresses.js
@@ -3,13 +3,15 @@
 import React from 'react'
 
 import WalletFace from 'components/WalletFace'
-import handle from 'utils/eventHandlers/handle'
 import getShortenedAddress from 'utils/wallets/getShortenedAddress'
+import { handle, ignoreEvent } from 'utils/eventHandlers'
 
 type Props = {|
-  +renameAddress: () => void,
+  +renameAddress: (Address) => void,
   +setActive: (addressIndex: Index) => void,
   +addresses: Addresses,
+  +addressNames: AddressNames,
+  +addressWalletNames: AddressNames,
   +isReadOnly: boolean,
 |}
 
@@ -17,22 +19,29 @@ const MnemonicAddresses = ({
   setActive,
   renameAddress,
   addresses,
+  addressNames,
+  addressWalletNames,
   isReadOnly,
 }: Props) => (
   <div className='mnemonic-addresses'>
-    {addresses.map((address, index) => (
-      <div key={address} className='address'>
-        <WalletFace
-          rename={renameAddress}
-          onClick={handle(setActive)(index)}
-          title={`Address #${index + 1}`}
-          description={getShortenedAddress(address)}
-          iconName='binding'
-          isReadOnly={isReadOnly}
-          hasActions
-        />
-      </div>
-    ))}
+    {addresses.map((address, index) => {
+      const walletName: string = addressWalletNames[address]
+      const addressName: string = walletName || addressNames[address]
+
+      return (
+        <div key={address} className='address'>
+          <WalletFace
+            onClick={handle(setActive)(index)}
+            rename={walletName ? null : ignoreEvent(renameAddress)(address)}
+            description={getShortenedAddress(address)}
+            title={addressName || `Address #${index + 1}`}
+            iconName='binding'
+            isReadOnly={isReadOnly}
+            hasActions={!walletName}
+          />
+        </div>
+      )
+    })}
   </div>
 )
 
