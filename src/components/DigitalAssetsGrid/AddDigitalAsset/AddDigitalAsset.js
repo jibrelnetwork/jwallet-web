52689af3542ffed0186492e0eb042ae6ca8afe65
Evgeny Vanchugov <dragonesis@ya.ru>
2018-10-28 17:08:39 +0300

    JWWEB-168 Create new AddDigitalAsset component

---
+++ src/components/DigitalAssetsGrid/AddDigitalAsset/AddDigitalAsset.js
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react'
+
+import { JIcon } from 'components/base'
+
+const AddAsset = () => (
+  <div className='add-asset'>
+    <div className='icon'>
+      <JIcon
+        size='medium'
+        color='gray'
+        name='priority-high'
+      />
+    </div>
+  </div>
+)
+
+export default AddAsset
