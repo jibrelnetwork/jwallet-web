f67188cc1349d8f7b6944c65805f26595e8655ba
Evgeny Vanchugov <dragonesis@ya.ru>
2018-10-31 17:50:29 +0300

    JWWEB-200 Create PopupButton component

--- src/components/PopupButton/PopupButton.js
+++ src/components/PopupButton/PopupButton.js
@@ -1,28 +1,66 @@
 // @flow
 
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 
-import { JIcon } from 'components/base'
+import { JIcon, JText } from 'components/base'
+import handle from 'utils/eventHandlers/handle'
 
 type Props = {
   +icon: string,
+  +informer: number,
 }
 
-class PopupButton extends PureComponent<Props> {
+type ComponentState = {|
+  +isActive: boolean,
+|}
+
+class PopupButton extends PureComponent<Props, ComponentState> {
+  static defaultProps = {
+    isActive: false,
+  }
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isActive: false,
+    }
+  }
+
+  toggle = (isActive: boolean) => {
+    this.setState({
+      isActive,
+    })
+  }
+
   render() {
     const {
       icon,
+      informer,
     } = this.props
 
+    const {
+      isActive,
+    } = this.state
+
     return (
-      <div className='popup-button'>
-        <div className='icon'>
-          <JIcon
-            size='medium'
-            color='gray'
-            name={icon}
-          />
+      <div className='popup-button' >
+        {informer && (
+          <div className='informer'>
+            <JText value={informer} size='small' color='white' weight='bold' />
+          </div>
+        )}
+        <div className='icon' onClick={handle(this.toggle)(!isActive)}>
+          <JIcon size='medium' color='gray' name={icon} />
         </div>
+        {isActive && (
+          <Fragment>
+            <div className='overlay' onClick={handle(this.toggle)(!isActive)} />
+            <div className='close' onClick={handle(this.toggle)(!isActive)}>
+              <JIcon size='medium' color='gray' name='cross' />
+            </div>
+          </Fragment>
+        )}
       </div>
     )
   }
