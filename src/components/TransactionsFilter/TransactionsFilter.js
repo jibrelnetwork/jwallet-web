c12560281ff95feec300dbae93d5ec78ba05e52b
Evgeny Vanchugov <dragonesis@ya.ru>
2018-11-16 12:37:08 +0300

    JWWEB-224 Implement component TransactionsFilter

---
+++ src/components/TransactionsFilter/TransactionsFilter.js
@@ -0,0 +1,51 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+
+import { PopupButton } from 'components'
+import { JText, JCheckbox } from 'components/base'
+
+type Props = {|
+  +setOnlyPending: ((boolean) => void),
+  +isOnlyPending: boolean,
+  +filterCount: number,
+|}
+
+class DigitalAssetsFilter extends PureComponent<Props> {
+  static defaultProps = {
+    isOnlyPending: false,
+    filterCount: 0,
+  }
+
+  render() {
+    const {
+      setOnlyPending,
+      isOnlyPending,
+      filterCount,
+    } = this.props
+
+    return (
+      <PopupButton icon={(filterCount === 0) ? 'filter' : 'filter-selected'} counter={filterCount}>
+        <div className='digital-assets-filter'>
+          <div className='title'>
+            <JText
+              color='gray'
+              size='normal'
+              value='Filter'
+              weight='bold'
+              whiteSpace='wrap'
+            />
+          </div>
+          <JCheckbox
+            onChange={setOnlyPending}
+            name='only-pending'
+            label='Only pending'
+            isChecked={isOnlyPending}
+          />
+        </div>
+      </PopupButton>
+    )
+  }
+}
+
+export default DigitalAssetsFilter
