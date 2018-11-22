eaf90a10294361fba3f90630dbfad06c1d408afd
Michael Maslov <flexxnn@gmail.com>
2018-11-22 15:13:27 +0300

    AssetBalance is now accepts BigNumber as balance

--- src/components/AssetBalance/AssetBalance.js
+++ src/components/AssetBalance/AssetBalance.js
@@ -1,30 +1,42 @@
 // @flow
 
 import React, { PureComponent } from 'react'
+import BigNumber from 'bignumber.js'
 
+import { divDecimals } from 'utils/numbers'
 import { JText } from 'components/base'
 
 type Props = {|
   +symbol: string,
   +color: 'blue' | 'gray',
   +size: 'small' | 'normal' | 'header',
-  +balance: number,
+  +balance: typeof BigNumber | string | number,
+  +decimals: number,
 |}
 
 class AssetBalance extends PureComponent<Props> {
   static defaultProps = {
     color: 'gray',
     size: 'normal',
+    decimals: 18,
   }
+
   render() {
     const {
       symbol,
       size,
       color,
       balance,
+      decimals,
     } = this.props
 
-    const balanceValue = balance.toLocaleString('en-US', { maximumFractionDigits: 2 })
+    if (!(balance instanceof BigNumber)) {
+      console.error(`AssetBalance: invalid balance type (required: BigNumber, expected: ${balance}`)
+      return null
+    }
+
+    const balanceValue = divDecimals(balance, decimals).toString()
+    // const balanceValue = balance.toLocaleString('en-US', { maximumFractionDigits: 2 })
 
     return (
       <div className='asset-balance'>
