e6b2a6a0985f63bd7d4ef0ec128c9462876024d7
Ivan Violentov <violentoff@gmail.com>
2018-11-22 19:36:34 +0300

    update utils/digitalAssets

--- src/utils/digitalAssets/index.js
+++ src/utils/digitalAssets/index.js
@@ -3,9 +3,11 @@
 import checkETH from './checkETH'
 import checkJNT from './checkJNT'
 import balanceToString from './balanceToString'
+import flattenDigitalAssets from './flattenDigitalAssets'
 
 export {
   checkETH,
   checkJNT,
   balanceToString,
+  flattenDigitalAssets,
 }
