bcdedd3fd7d8eb7f819f61ad24b095f47268b1df
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2018-11-30 11:07:00 +0300

    Update storybook
    Bump storybook version
    Update storybook config
    Add static storybook build command
    Add storybook docs dir to .gitignore

--- .buildscripts/clear.js
+++ .buildscripts/clear.js
@@ -1,11 +1,14 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-console.log(`Clear: emptying "${path.resolve('../build')}"...`)
+console.log(`Clear: emptying build directories...`)
 
 try {
   fs.emptyDirSync(
     path.resolve('build')
+  )
+  fs.emptyDirSync(
+    path.resolve('docs/storybook')
   )
 }
 catch (err) {
