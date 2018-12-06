0fb8267fc04673497b7afc29264698f0465b0af3
Ivan Violentov <violentoff@gmail.com>
2018-12-06 18:07:50 +0300

    add types for app config

---
+++ flow-typed/config.js
@@ -0,0 +1,56 @@
+// @flow
+
+declare type QRCodeAppearanceConfig = {|
+  +size: number,
+  +errorCorrectionLevel: string,
+  +color: {|
+    +light: string,
+    +dark: string,
+  |},
+|}
+
+declare type JSONFormatConfig = {|
+  +type: string,
+  +size: number,
+|}
+
+declare type AppConfig = {|
+  +searchTimeout: number,
+  +resetIncorrectPincodeTimeout: number,
+  +modalOpeningClosingTimeout: number,
+  +popoverClickTimeout: number,
+  +popoverOpeningClosingTimeout: number,
+  +defaultAppearableTimeout: number,
+  +mobileWidth: number,
+  +accountsLoadingCount: number,
+  +popoverShakeTimeout: number,
+  +qrCodeDefaultAppearance: QRCodeAppearanceConfig,
+  +urlRe: RegExp,
+  +modalShakeTimeout: number,
+  +addressesPerIteration: number,
+  +getBalancesIntervalTimeout: number,
+  +getTransactionsIntervalTimeout: number,
+  +defaultDecimals: number,
+  +jsonFormat: JSONFormatConfig,
+  +blockExplorerApiOptions: RequestOptions,
+  +maxPasswordLength: number,
+  +delayBeforeFormClean: number,
+  +defaultDerivationPath: string,
+  +balanceLoadingTimeout: number,
+  +copyToBufferTimeout: number,
+  +walletIdInitTimeout: number,
+  +blockExplorerLink: 'etherscan.io',
+  +supportedLanguages: LanguageCode[],
+  +mnemonicAddressesCount: number,
+  +landingURL: 'https://jwallet.network',
+  +deleteConfirmTimeout: number,
+  +minTransactionsCountToShow: number,
+  +latestBlockSyncTimeout: number,
+  +currentBlockSyncTimeout: number,
+  +processingBlockWaitTimeout: number,
+  +syncTransactionsTimeout: number,
+  +maxBlocksPerTransactionsRequest: number,
+  +minBlocksPerTransactionsRequest: number,
+  +blockExplorerMaxResponseSize: number,
+  +requestQueueWorkersCount: number,
+|}
