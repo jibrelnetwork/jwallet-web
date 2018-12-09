dc6128fcff1a50d286b82de7289da3dbb1858f9d
Ivan Violentov <violentoff@gmail.com>
2018-12-09 21:04:05 +0300

    fix funds types

--- flow-typed/redux/funds.js
+++ flow-typed/redux/funds.js
@@ -1,39 +1,49 @@
 // @flow
 
 declare type SendFundsData = {
-    +invalidFields: FormFields,
-    +alert: string,
-    +amount: string,
-    +assetAddress: string,
-    +recipient: Address,
-    +password: string,
-    +currentStep: number,
-    +gas?: string,
-    +gasPrice?: string,
-    +nonce?: string,
-  }
+  +invalidFields: FormFields,
+  +alert: string,
+  +amount: string,
+  +assetAddress: string,
+  +recipient: Address,
+  +password: string,
+  +currentStep: number,
+  +gas?: string,
+  +gasPrice?: string,
+  +nonce?: string,
+}
 
-  declare type ReceiveFundsData = {
-    +invalidFields: FormFields,
-    +assetAddress: Address,
-    +amount: string,
-    +isCopied: boolean,
-  }
+declare type SendTransactionProps = {
+  +value: string,
+  +privateKey: string,
+  +contractAddress: string,
+  +recipient: OwnerAddress,
+  +gas: string,
+  +nonce: string,
+  +gasPrice: string,
+}
 
-  declare type ConvertFundsData = {
-    +invalidFields: FormFields,
-    +fromAsset: string,
-    +fromAmount: string,
-    +toAsset: string,
-    +toAmount: string,
-  }
+declare type ReceiveFundsData = {
+  +invalidFields: FormFields,
+  +assetAddress: Address,
+  +amount: string,
+  +isCopied: boolean,
+}
 
-  declare type TXData = {
-    to: Address,
-    value: Bignumber,
-    privateKey: string,
-    contractAddress?: Address,
-    gasPrice?: Bignumber,
-    gasLimit?: Bignumber,
-    nonce?: Bignumber,
-  }
+declare type ConvertFundsData = {
+  +invalidFields: FormFields,
+  +fromAsset: string,
+  +fromAmount: string,
+  +toAsset: string,
+  +toAmount: string,
+}
+
+declare type TXData = {
+  to: Address,
+  value: Bignumber,
+  privateKey: string,
+  contractAddress?: Address,
+  gasPrice?: Bignumber,
+  gasLimit?: Bignumber,
+  nonce?: Bignumber,
+}
