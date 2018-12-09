836b25de8fd5f857a220322f2ef407c2e4ed6fc3
Ivan Violentov <violentoff@gmail.com>
2018-12-09 21:00:30 +0300

    update utils/transactions

--- src/utils/transactions/getTxFee.js
+++ src/utils/transactions/getTxFee.js
@@ -2,13 +2,12 @@
 
 import { BigNumber } from 'bignumber.js'
 
-function getTxFee(gasUsed: string, gasPrice: string, decimals: number): string {
+function getTxFee(gasUsed: number, gasPrice: string, decimals: number): string {
   const base: BigNumber = new BigNumber(10)
   const basePow: BigNumber = base.pow(decimals)
-  const gasUsedBN: BigNumber = new BigNumber(gasUsed)
   const gasPriceBN: BigNumber = new BigNumber(gasPrice)
 
-  return gasUsedBN.times(gasPriceBN).div(basePow).toFormat()
+  return gasPriceBN.times(gasUsed).div(basePow).toFormat()
 }
 
 export default getTxFee
