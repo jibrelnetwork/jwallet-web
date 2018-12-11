ed003c42a827107f3d463d672456c5bf192e31c8
Michael Maslov <flexxnn@gmail.com>
2018-12-11 14:03:08 +0300

    Fix big number patch

--- src/utils/numbers/index.js
+++ src/utils/numbers/index.js
@@ -1,10 +1,10 @@
 // @flow
 
+import './bigNumber'
 import round from './round'
 import isZero from './isZero'
 import divDecimals from './divDecimals'
 import divideThousands from './divideThousands'
-import './bigNumber'
 
 export {
   round,
