b60958fea9f733f5fbe71abaa875a095cb8c869e
Ivan Violentov <violentoff@gmail.com>
2018-12-18 13:05:29 +0300

    update wallets utils

--- src/utils/wallets/index.js
+++ src/utils/wallets/index.js
@@ -1,13 +1,21 @@
 // @flow
 
+import add0x from './add0x'
+import strip0x from './strip0x'
 import getWallet from './getWallet'
+import checkAddressValid from './checkAddressValid'
 import checkMnemonicType from './checkMnemonicType'
 import getShortenedAddress from './getShortenedAddress'
-import getAddressWalletNames from './getAddressWalletNames'
+import getAddressWithChecksum from './getAddressWithChecksum'
+import checkAddressWithChecksumValid from './checkAddressWithChecksumValid'
 
 export {
+  add0x,
+  strip0x,
   getWallet,
+  checkAddressValid,
   checkMnemonicType,
   getShortenedAddress,
-  getAddressWalletNames,
+  getAddressWithChecksum,
+  checkAddressWithChecksumValid,
 }
