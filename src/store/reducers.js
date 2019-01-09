9d87d57b5e4c312d0cb8760759ad1127d6af4a98
Ivan Violentov <violentoff@gmail.com>
2019-01-09 18:56:04 +0300

    JWWEB-188: Update redux-persist management

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,9 @@
 // @flow
 
-import storage from 'localforage'
 import { combineReducers, type Reducer } from 'redux'
-import { persistReducer } from 'redux-persist'
 import { routerReducer as router } from 'react-router-redux'
 
-import { type AppAction } from 'routes'
+import type { AppAction } from 'routes'
 
 // wallets
 import wallets from 'routes/Wallets/modules/wallets'
@@ -15,15 +13,13 @@ import walletsBackup from 'routes/Wallets/routes/Backup/modules/walletsBackup'
 import walletsAddresses from 'routes/Wallets/routes/Addresses/modules/walletsAddresses'
 import walletsRenameAddress from 'routes/Wallets/routes/RenameAddress/modules/walletsRenameAddress'
 
-// Digital ssets
+// digital assets
 import digitalAssets from 'routes/DigitalAssets/modules/digitalAssets'
-import addAsset from 'routes/DigitalAssets/routes/AddAsset/modules/addAsset'
-import editAsset from 'routes/DigitalAssets/routes/EditAsset/modules/editAsset'
+import digitalAssetsAdd from 'routes/DigitalAssets/routes/AddAsset/modules/addAsset'
+import digitalAssetsEdit from 'routes/DigitalAssets/routes/EditAsset/modules/editAsset'
+import digitalAssetsSend from 'routes/DigitalAssets/routes/Send/modules/digitalAssetsSend'
 import digitalAssetsGrid from 'routes/DigitalAssets/routes/Grid/modules/digitalAssetsGrid'
 import digitalAssetsManage from 'routes/DigitalAssets/routes/Manage/modules/digitalAssetsManage'
-
-// Send digital asset
-import digitalAssetsSend from 'routes/DigitalAssets/routes/Send/modules/digitalAssetsSend'
 
 // networks
 import networks from 'routes/modules/networks'
@@ -46,55 +42,38 @@ import settings from 'routes/Settings/modules/settings'
 // favorites
 import favorites from 'routes/Favorites/modules/favorites'
 
-const persistConfig = {
-  storage,
-  key: 'jwallet-web',
-  whitelist: [
-    'blocks',
-    'wallets',
-    'balances',
-    'comments',
-    'favorites',
-    'transactions',
-    'digitalAssets',
-    'walletsAddresses',
-  ],
-}
+import persistReducers from './persistReducers'
 
 export function makeRootReducer() {
   const rootReducer: Reducer<AppState, AppAction> = combineReducers({
     // wallets
-    wallets,
     walletsCreate,
     walletsImport,
     walletsBackup,
-    walletsAddresses,
     walletsRenameAddress,
-    // networks
-    networks,
     // digitalAssets
-    digitalAssets,
+    digitalAssetsAdd,
+    digitalAssetsEdit,
+    digitalAssetsSend,
     digitalAssetsGrid,
     digitalAssetsManage,
-    addAsset,
-    editAsset,
-    // send asset
-    digitalAssetsSend,
-    // blocks
-    blocks,
-    // balances
-    balances,
-    // comments
-    comments,
-    // transactions
-    transactions,
     // settings
     settings,
-    // favorites
-    favorites,
     // router
     router,
+    // persist
+    ...persistReducers({
+      blocks,
+      wallets,
+      balances,
+      comments,
+      networks,
+      favorites,
+      transactions,
+      digitalAssets,
+      walletsAddresses,
+    }),
   })
 
-  return persistReducer/* :: < AppState, AppAction > */(persistConfig, rootReducer)
+  return rootReducer
 }
