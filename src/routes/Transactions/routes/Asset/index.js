c2db1247631fedc7fd15bc243cffd3611426031f
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-01-10 12:36:24 +0300

    JWWEB-26: Custom pageview for asset transactions

--- src/routes/Transactions/routes/Asset/index.js
+++ src/routes/Transactions/routes/Asset/index.js
@@ -1,8 +1,15 @@
 // @flow
 
+import { ga } from 'utils/analytics'
 import View from './TransactionsAssetViewContainer'
 
 export default {
   path: ':asset',
+  onEnter: (nextState: ReactRouterState): void =>
+    ga(
+      'send',
+      'pageview',
+      nextState.location.pathname
+    ),
   component: View,
 }
