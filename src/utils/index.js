ded2fa470c8be332e6da7b1ee629e3285de5678f
Michael Maslov <flexxnn@gmail.com>
2019-01-16 11:27:40 +0300

    small fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 // @flow
 
-import * as ethUtils from './convert'
 import * as i18nUtils from './i18n'
 import * as timeUtils from './time'
 import * as errorsUtils from './errors'
@@ -14,7 +13,6 @@ import * as digitalAssetsUtils from './digitalAssets'
 import * as eventHandlersUtils from './eventHandlers'
 
 export {
-  ethUtils,
   i18nUtils,
   timeUtils,
   errorsUtils,
