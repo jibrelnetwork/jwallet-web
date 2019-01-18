3d25d58443d744b80fa2592ea88bbb642b6d5aae
Ivan Violentov <violentoff@gmail.com>
2019-01-18 14:28:32 +0300

    Fix getAddressLink

--- src/utils/transactions/getAddressLink.js
+++ src/utils/transactions/getAddressLink.js
@@ -2,12 +2,10 @@
 
 import config from 'config'
 
-function getAddressLink(address: ?Address, blockExplorerUISubdomain: string): string {
+function getAddressLink(address: Address, blockExplorerUISubdomain: string): string {
   const uiSubdomainPrefix: string = blockExplorerUISubdomain ? `${blockExplorerUISubdomain}.` : ''
 
-  return address
-    ? `//${uiSubdomainPrefix}${config.blockExplorerUILink}/address/${address}`
-    : ''
+  return `//${uiSubdomainPrefix}${config.blockExplorerUILink}/address/${address}`
 }
 
 export default getAddressLink
