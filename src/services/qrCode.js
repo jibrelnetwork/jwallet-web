a72650da2f498169337cc4bb1c6702625dc05a3d
Anton Yessaulenko <2114572+Creepypoke@users.noreply.github.com>
2019-01-28 17:56:13 +0300

    JWWEB-208 Error for incorrect selector was added

--- src/services/qrCode.js
+++ src/services/qrCode.js
@@ -9,13 +9,14 @@ function generate(payload: { requisites: any, appearance: any, selector: string 
   const options = { ...config.qrCodeDefaultAppearance, ...appearance }
 
   const wrapper = document.querySelector(selector)
-  if (wrapper) {
-    wrapper.innerHTML = ''
-    const canvas = document.createElement('canvas')
-    wrapper.appendChild(canvas)
-
-    QRCode.toCanvas(canvas, `ethereum:${requisites.to}?token=ETH`, options)
+  if (!wrapper) {
+    throw new Error('Selector is incorrect')
   }
+  wrapper.innerHTML = ''
+  const canvas = document.createElement('canvas')
+  wrapper.appendChild(canvas)
+
+  QRCode.toCanvas(canvas, `ethereum:${requisites.to}?token=ETH`, options)
 }
 
 export default { generate }
