0c14106984dcdf8c25eff3b8bf3f5d19883854a3
Evgeny Vanchugov <dragonesis@ya.ru>
2019-01-28 22:07:49 +0300

    Change styles jThumbnail and OverlayNotification, fix style not found page

--- src/routes/NotFound/NotFoundView.js
+++ src/routes/NotFound/NotFoundView.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 import { JThumbnail, JFlatButton } from 'components/base'
 
@@ -8,31 +8,57 @@ type Props = {|
   goToHome: () => void,
 |}
 
-const NotFoundView = ({ goToHome }: Props) => (
-  <div className='not-found-view'>
-    <div className='content'>
-      <JThumbnail
-        color='white'
-        iconSize='xlarge'
-        image='auth-question'
-        title='404. Page Not Found.'
-        description={[
-          'The page you\'re looking for can\'t be found.',
-          'Check the URL and try again.',
-        ]}
-      />
-      <div className='actions'>
-        <div className='back'>
-          <JFlatButton
-            onClick={goToHome}
+type StateProps = {|
+  isHovered: boolean,
+|}
+
+class NotFoundView extends PureComponent<Props, StateProps> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isHovered: false,
+    }
+  }
+
+  onHover = () => this.setState({ isHovered: !this.state.isHovered })
+
+  render() {
+    const {
+      goToHome,
+    } = this.props
+
+    const {
+      isHovered,
+    }: StateProps = this.state
+
+    return (
+      <div className='not-found-view'>
+        <div className='content'>
+          <JThumbnail
             color='white'
-            label='Back to Home'
-            isHoverOpacity
+            iconSize='xlarge'
+            image='auth-question'
+            title='404. Page Not Found.'
+            isTransparent
+            description={[
+              'The page you\'re looking for can\'t be found.',
+              'Check the URL and try again.',
+            ]}
           />
+          <div className='actions'>
+            <div className='back' onMouseEnter={this.onHover} onMouseLeave={this.onHover}>
+              <JFlatButton
+                onClick={goToHome}
+                color={isHovered ? 'sky' : 'white'}
+                label='Back to Home'
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default NotFoundView
