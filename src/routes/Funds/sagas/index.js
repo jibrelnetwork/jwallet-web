9f6e056b763a69d015efe1b57ac950b9fd4ec961
Ivan Violentov <violentoff@gmail.com>
2019-01-28 19:11:43 +0300

    Remove legacy code

--- src/routes/Funds/sagas/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import * as sendFunds from '../routes/Send/sagas/sendFunds'
-import * as receiveFunds from '../routes/Receive/sagas/receiveFunds'
-
-export default {
-  ...sendFunds,
-  ...receiveFunds,
-}
