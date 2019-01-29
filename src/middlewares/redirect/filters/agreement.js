a8c01e9174f3bfd2989763851034b41db86bb106
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-01-29 19:27:39 +0300

    Hotfix

--- src/middlewares/redirect/filters/agreement.js
+++ src/middlewares/redirect/filters/agreement.js
@@ -7,7 +7,7 @@ export const agreement = (state: AppState, pathname: string): string => {
   const isAgreementsPathname = /^\/agreements\/?$/.test(pathname)
   const isAllAgreementsChecked = checkAgreements(CONDITIONS_LIST)
   if (isAgreementsPathname && isAllAgreementsChecked) {
-    return '/wallets'
+    return '/'
   }
   if (!isAgreementsPathname && !isAllAgreementsChecked) {
     return '/agreements'
