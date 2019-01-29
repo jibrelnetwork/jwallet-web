4108b4467c68f97e622b7bf0c8e17a53d1aab0ac
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-01-29 18:49:27 +0300

    JWWEB-437: Add filter for agreement routing

---
+++ src/data/agreements.js
@@ -0,0 +1,6 @@
+export const CONDITIONS_LIST = [
+  'understandPrivateDataPolicy',
+  'consentNoWarranty',
+  'consentTrackingCookies',
+  'acceptTermsAndConditions',
+]
--- src/middlewares/redirect/filters/agreement.js
+++ src/middlewares/redirect/filters/agreement.js
@@ -1,1 +1,15 @@
-export const agreement = (state, pathname) => pathname
+import { checkAgreements } from 'utils/agreements'
+import { CONDITIONS_LIST } from 'data/agreements'
+
+export const agreement = (state, pathname) => {
+  const isAgreementsPathname = /^\/agreements\/?$/.test(pathname)
+  const isAllAgreementsChecked = checkAgreements(CONDITIONS_LIST)
+  if (isAgreementsPathname && isAllAgreementsChecked) {
+    return '/wallets'
+  }
+  if (!isAgreementsPathname && !isAllAgreementsChecked) {
+    return '/agreements'
+  }
+
+  return pathname
+}
