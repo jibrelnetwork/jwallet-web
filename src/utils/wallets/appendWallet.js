ece295cda6b1531c6b49e0d418aafb82dfcf24f3
Ivan Violentov <violentoff@gmail.com>
2019-01-29 15:15:20 +0300

    Implement sorting of wallets on list update

--- src/utils/wallets/appendWallet.js
+++ src/utils/wallets/appendWallet.js
@@ -1,10 +1,18 @@
 // @flow
 
 function appendWallet(wallets: Wallets, wallet: Wallet): Wallets {
+  /* eslint-disable-next-line fp/no-mutating-methods */
   return [
     ...wallets,
     wallet,
-  ]
+  ].sort((a: Wallet, b: Wallet): number => {
+    if (a.orderIndex === b.orderIndex) {
+      return 0
+    }
+
+    return (a.orderIndex > b.orderIndex) ? 1 : -1
+  })
+  /* eslint-enable-next-line fp/no-mutating-methods */
 }
 
 export default appendWallet
