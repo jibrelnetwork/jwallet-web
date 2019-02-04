86de689f073a487f5b14180d364db4c012d353c1
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-02-04 19:58:18 +0300

    JWWEB-493: Fix asset symbol styles and behavior

--- src/components/base/JAssetSymbol/JAssetSymbol.js
+++ src/components/base/JAssetSymbol/JAssetSymbol.js
@@ -2,8 +2,13 @@
 
 import React from 'react'
 
+import { SYMBOLS_AVAILABLE_CLASS_INDEX } from './symbolsAvailable'
+
+const getSymbolClassName = symbol =>
+  SYMBOLS_AVAILABLE_CLASS_INDEX[symbol.toLowerCase()] || '-symbol-not-listed'
+
 const JAssetSymbol = ({ symbol, color }: Props) => (
-  <div className={`j-asset-symbol -symbol-${symbol.toLowerCase()} -${color}`} />
+  <div className={`j-asset-symbol ${getSymbolClassName(symbol)} -${color}`} data-symbol={symbol} />
 )
 
 type Props = {
