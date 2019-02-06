bcc3260e7735039d0c08ec8c9128ad5efcfc8c0f
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-02-06 12:21:29 +0300

    Fix assets management toggle

--- src/data/assets/index.js
+++ src/data/assets/index.js
@@ -1,11 +1,28 @@
 // @flow
 
+import { getAddressChecksum } from 'utils/address'
+
 import ethereum from './ethereum'
-import mainnet from '../../../assets/mainnet/assets.json'
-import ropsten from '../../../assets/ropsten/assets.json'
+import rawMainnet from '../../../assets/mainnet/assets.json'
+import rawRopsten from '../../../assets/ropsten/assets.json'
+
+// FIXME: this should be added one-time during build of the assets list
+const addChecksum = (asset) => {
+  if (asset.blockchainParams && asset.blockchainParams.address) {
+    return {
+      ...asset,
+      blockchainParams: {
+        ...asset.blockchainParams,
+        address: getAddressChecksum(asset.blockchainParams.address),
+      },
+    }
+  }
+
+  return asset
+}
 
 export default {
-  mainnet,
-  ropsten,
+  mainnet: rawMainnet.map(asset => addChecksum(asset)),
+  ropsten: rawRopsten.map(asset => addChecksum(asset)),
   ethereum,
 }
