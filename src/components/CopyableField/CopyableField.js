3439a5c99d25435128800f45ea5f373337350ba2
Evgeny Vanchugov <dragonesis@ya.ru>
2019-02-06 15:50:36 +0300

    JWWEB-475 The 'Copy Backup phrase' button doesn't change by hovering and copying

--- src/components/CopyableField/CopyableField.js
+++ src/components/CopyableField/CopyableField.js
@@ -1,12 +1,13 @@
 // @flow
 
 import React, { PureComponent } from 'react'
+import classNames from 'classnames'
 
-import JText from 'components/base/JText'
+import { JText, JIcon } from 'components/base'
 import handle from 'utils/eventHandlers/handle'
 import OverlayActions from 'components/OverlayActions'
 
-type CopyableFieldHandler = () => void
+type CopyableFieldHandler = (value: string) => void
 
 type Props = {|
   +copy: CopyableFieldHandler,
@@ -15,10 +16,41 @@ type Props = {|
   +valueToDisplay: ?string,
 |}
 
-class CopyableField extends PureComponent<Props> {
+type StateProps = {|
+  isSuccess: boolean,
+|}
+
+class CopyableField extends PureComponent<Props, StateProps> {
   static defaultProps = {
     download: null,
     valueToDisplay: null,
+  }
+
+  // eslint-disable-next-line react/sort-comp
+  toggleTimeout: ?TimeoutID = null
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isSuccess: false,
+    }
+  }
+
+  onClick = (copy: CopyableFieldHandler, value: string) => {
+    this.setState({ isSuccess: true })
+
+    this.toggleTimeout = setTimeout(() => {
+      this.setState({ isSuccess: false })
+    }, 2000)
+
+    return (copy)(value)
+  }
+
+  componentWillUnmount() {
+    if (this.toglleTimeout) {
+      clearTimeout(this.toggleTimeout)
+    }
   }
 
   render() {
@@ -29,8 +61,12 @@ class CopyableField extends PureComponent<Props> {
       valueToDisplay,
     }: Props = this.props
 
+    const {
+      isSuccess,
+    }: StateProps = this.state
+
     return (
-      <div className='copyable-field'>
+      <div className={classNames('copyable-field', isSuccess && '-success')}>
         <div className='value'>
           <JText
             value={valueToDisplay || value}
@@ -42,11 +78,20 @@ class CopyableField extends PureComponent<Props> {
         </div>
         <div className='overlay'>
           <OverlayActions
-            copy={handle(copy)(value)}
+            // copy={handle(copy)(value)}
+            copy={handle(this.onClick)(copy, value)}
             load={download ? handle(download)(value) : null}
             loadLabel='Download as TXT'
             copyLabel='Copy backup phrase'
           />
+        </div>
+        <div className='success'>
+          <div className='icon'>
+            <JIcon name='check-circle' color='white' size='medium' />
+          </div>
+          <div className='text'>
+            <JText value='Copied!' color='white' weight='bold' />
+          </div>
         </div>
       </div>
     )
