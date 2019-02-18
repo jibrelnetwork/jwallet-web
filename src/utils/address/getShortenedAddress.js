91afab4e4330fe04b5cdd0fbcb30f8f26894bcbd
Ivan Violentov <violentoff@gmail.com>
2019-02-18 13:26:32 +0300

    Example of fix

--- src/utils/address/getShortenedAddress.js
+++ src/utils/address/getShortenedAddress.js
@@ -4,7 +4,7 @@ function getShortenedAddress(
   address: Address,
   startSymbols: number = 15,
   finishSymbols: number = 6,
-  splitSymbol: string = '...'
+  splitSymbol: string = '...',
 ): string {
   return `${address.substr(0, startSymbols)}${splitSymbol}${address.substr(-1 * finishSymbols)}`
 }
