7732c5a84301c764a5eeb215bf6df312104b56d0
Ivan Violentov <violentoff@gmail.com>
2019-02-28 15:07:34 +0000

    Exclude private methods from PromiseWorker

--- src/workers/PromiseWorker.js
+++ src/workers/PromiseWorker.js
@@ -36,83 +36,93 @@ const TASK_TIMEOUT = 30 * 1000
 const MAX_QUEUE_LENGTH = 1000
 const WORKER_TYPE = 'promise'
 
+/**
+ * Private methods for PromiseWorker
+ */
+
+function removeTask(self: Object, taskId: string) {
+  const taskIds: string[] = Object.keys(self.queue)
+
+  self.queue = taskIds.reduce((reduceResult: PromiseWorkerQueue, currentTaskId: string) => {
+    if (taskId === currentTaskId) {
+      return reduceResult
+    }
+
+    reduceResult[currentTaskId] = self.queue[currentTaskId]
+
+    return reduceResult
+  }, {})
+}
+
+function addTask(self: Object, taskId: string, task: PromiseWorkerTask) {
+  const queueLength = Object.keys(self.queue).length
+  const existedTask = self.queue[taskId]
+
+  if (queueLength > MAX_QUEUE_LENGTH) {
+    task.reject(new WorkerQueueExceededError())
+  } else if (existedTask) {
+    existedTask.reject(new WorkerTaskReplacedError())
+  }
+
+  self.queue[taskId] = task
+
+  setTimeout(() => {
+    removeTask(self, taskId)
+    const timeoutErrorMsg = task.errorMessage || `Worker did not respond within ${TASK_TIMEOUT}ms`
+    task.reject(new WorkerTaskTimeoutError(timeoutErrorMsg))
+  }, TASK_TIMEOUT)
+}
+
+function handleError(err: Error) {
+  throw new WorkerError(err, WORKER_TYPE)
+}
+
+function handleTask(self, {
+  error,
+  taskId,
+  payload,
+}: PromiseWorkerTaskResult) {
+  const task: ?PromiseWorkerTask = self.queue[taskId]
+
+  if (!task) {
+    return
+  }
+
+  if (error) {
+    task.reject(new WorkerTaskError(payload))
+  } else {
+    task.resolve(payload)
+  }
+
+  removeTask(self, taskId)
+}
+
+function startListen(self: Object, worker: Object) {
+  if (self.worker) {
+    throw new WorkerError('Worker has been already started', WORKER_TYPE)
+  }
+
+  self.worker = worker
+  self.worker.onerror = err => handleError(err)
+  self.worker.onmessage = msg => handleTask(self, msg.data)
+}
+
+function endListen(self: Object) {
+  if (!self.worker) {
+    return
+  }
+
+  self.worker.onerror = null
+  self.worker.onmessage = null
+}
+
 class PromiseWorker {
-  queue: PromiseWorkerQueue
   worker: ?Object
+  queue: PromiseWorkerQueue
 
   constructor(worker: Object) {
     this.queue = {}
-    this.startListen(worker)
-  }
-
-  handleError = (err: Error) => {
-    throw new WorkerError(err, WORKER_TYPE)
-  }
-
-  handleTask = ({
-    error,
-    taskId,
-    payload,
-  }: PromiseWorkerTaskResult) => {
-    const task: ?PromiseWorkerTask = this.queue[taskId]
-
-    if (!task) {
-      return
-    }
-
-    if (error) {
-      task.reject(new WorkerTaskError(payload))
-    } else {
-      task.resolve(payload)
-    }
-
-    this.removeTask(taskId)
-  }
-
-  startListen = (worker: Object) => {
-    this.worker = worker
-    this.worker.onerror = err => this.handleError(err)
-    this.worker.onmessage = msg => this.handleTask(msg.data)
-  }
-
-  endListen = () => {
-    if (this.worker) {
-      this.worker.onerror = null
-      this.worker.onmessage = null
-    }
-  }
-
-  addTask = (taskId: string, task: PromiseWorkerTask) => {
-    const queueLength = Object.keys(this.queue).length
-    const existedTask = this.queue[taskId]
-
-    if (queueLength > MAX_QUEUE_LENGTH) {
-      task.reject(new WorkerQueueExceededError())
-    } else if (existedTask) {
-      existedTask.reject(new WorkerTaskReplacedError())
-    }
-
-    this.queue[taskId] = task
-
-    setTimeout(() => {
-      this.removeTask(taskId)
-      const timeoutErrorMsg = task.errorMessage || `Worker did not respond within ${TASK_TIMEOUT}ms`
-      task.reject(new WorkerTaskTimeoutError(timeoutErrorMsg))
-    }, TASK_TIMEOUT)
-  }
-
-  removeTask = (taskId: string) => {
-    const taskIds: string[] = Object.keys(this.queue)
-
-    this.queue = taskIds.reduce((reduceResult: PromiseWorkerQueue, currentTaskId: string) => {
-      if (taskId === currentTaskId) {
-        return reduceResult
-      }
-
-      reduceResult[currentTaskId] = this.queue[currentTaskId]
-
-      return reduceResult
-    }, {})
+    startListen(this, worker)
   }
 
   executeTask = ({
@@ -137,7 +147,7 @@ class PromiseWorker {
       this.worker.postMessage(msgData)
     }
 
-    return new Promise((resolve, reject) => this.addTask(taskId, {
+    return new Promise((resolve, reject) => addTask(this, taskId, {
       reject,
       resolve,
       errorMessage,
@@ -146,7 +156,7 @@ class PromiseWorker {
 
   terminate = () => {
     this.queue = {}
-    this.endListen()
+    endListen(this)
 
     if (this.worker) {
       this.worker.terminate()
@@ -154,13 +164,13 @@ class PromiseWorker {
     }
   }
 
-  restart = (worker: Object) => {
-    if (this.worker === worker) {
+  restart = (newWorker: Object) => {
+    if (this.worker === newWorker) {
       throw new WorkerError('Can not restart the same worker instance', WORKER_TYPE)
     }
 
     this.terminate()
-    this.startListen(worker)
+    startListen(this, newWorker)
   }
 }
 
