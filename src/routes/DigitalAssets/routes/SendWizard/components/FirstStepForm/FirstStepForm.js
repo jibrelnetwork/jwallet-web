321d68c3cc671ad5839daff0b916ad96a3fe5e9d
Michael Maslov <michael.maslov@jibrel.network>
2019-03-07 15:58:19 +0300

    add yup and getRecipientWarning

--- src/routes/DigitalAssets/routes/SendWizard/components/FirstStepForm/FirstStepForm.js
+++ src/routes/DigitalAssets/routes/SendWizard/components/FirstStepForm/FirstStepForm.js
@@ -1,17 +1,20 @@
 // @flow strict
 
-import * as React from 'react'
+import React, { Component } from 'react'
 import classNames from 'classnames'
 import { t } from 'ttag'
+import { FORM_ERROR } from 'final-form'
 import {
   Form,
   Field,
 } from 'react-final-form'
+import * as yup from 'yup'
 
 import {
   JInputField,
   JRaisedButton,
 } from 'components/base'
+import { checkAddressValid } from 'utils/address'
 
 import {
   type FirstStepValues,
@@ -19,74 +22,138 @@ import {
 
 import styles from './firstStepForm.m.scss'
 
-type SaveValuesFunction = (values: FirstStepValues) => Promise<void>
+type OnSubmit = (values: FirstStepValues) => Promise<void>
 
 type Props = {|
-  saveValues: SaveValuesFunction,
+  onSubmit: OnSubmit,
   initialValues: FirstStepValues,
 |}
 
-const validateAndSubmit =
-  (saveValues: SaveValuesFunction) =>
-    async (values: FirstStepValues) => {
-      await saveValues(values)
+const MSG_REQUIRED = t`This field is required`
+const MSG_INVALID_ADDRESS = t`Invalid address`
+const MSG_INVALID_AMOUNT = t`Invalid amount`
+
+const checkAddress = yup
+  .string()
+  .required(MSG_REQUIRED)
+  .test(
+    'isAddress',
+    MSG_INVALID_ADDRESS,
+    address => checkAddressValid(address),
+  )
+
+const checkAssetAddress = yup
+  .string()
+  .required(MSG_REQUIRED)
+  .test(
+    'isAssetAddress',
+    MSG_INVALID_ADDRESS,
+    address => address === 'Ethereum' || checkAddressValid(address),
+  )
+
+class FirstStepForm extends Component<Props> {
+  getRecipientWarning = (values: FirstStepValues) => {
+    if (values.recipient === values.owner) {
+      return t`Recipient address is the same as sender address`
     }
 
-function FirstStepForm({
-  saveValues,
-  initialValues,
-}: Props) {
-  return (
-    <Form
-      initialValues={initialValues}
-      onSubmit={validateAndSubmit(saveValues)}
-      render={({
-        handleSubmit, form, submitting, submitError,
-      }) => (
-        <form>
-          <div className={classNames(
-            styles.form,
-            { [styles.submitting]: submitting },
-          )}
-          >
-            {submitError && <div className='error'>{submitError}</div>}
-            <Field
-              component={JInputField}
-              name='owner'
-              color='gray'
-              label={t`Current address`}
-              isDisabled
-            />
-            <Field
-              component={JInputField}
-              name='recipient'
-              color='gray'
-              placeholder={t`Recipient address`}
-            />
-            <Field
-              component={JInputField}
-              name='asset'
-              color='gray'
-              label={t`Asset address`}
-            />
-            <Field
-              component={JInputField}
-              name='amount'
-              color='gray'
-              label={t`Amount`}
-            />
-            <JRaisedButton
-              onClick={handleSubmit}
-              color='blue'
-              label={t`Confirm`}
-              labelColor='white'
-              isWide
-            />
-          </div>
-        </form>
-      )}
-    />
-  )
+    return ''
+  }
+
+  handleSubmit = async (values: FirstStepValues) => {
+    try {
+      const validationSchema = yup.object().shape({
+        owner: checkAddress,
+        recipient: checkAddress,
+        asset: checkAssetAddress,
+        amount: yup
+          .number()
+          .typeError(MSG_INVALID_AMOUNT)
+          .required(MSG_REQUIRED)
+          .positive(MSG_INVALID_AMOUNT),
+        // TODO: .max(<this asset balace + gasPrice*gasLimit>,
+        // t`Amount plus blockchain fee exceeds balance`)
+      })
+
+      await validationSchema.validate(values, { abortEarly: false })
+      await this.props.onSubmit(values)
+
+      return null
+    } catch (err) {
+      if (err instanceof yup.ValidationError) {
+        const errors = err.inner.reduce((result, error) => ({
+          ...result,
+          [error.path]: error.message,
+        }), {})
+
+        return errors
+      }
+
+      return {
+        [FORM_ERROR]: t`Something wrong (${err.message})`,
+      }
+    }
+  }
+
+  render() {
+    const {
+      initialValues,
+    } = this.props
+
+    return (
+      <Form
+        initialValues={initialValues}
+        onSubmit={this.handleSubmit}
+        render={({
+          handleSubmit, form, submitting, submitError, values,
+        }) => (
+          <form>
+            <div className={classNames(
+              styles.form,
+              { [styles.submitting]: submitting },
+            )}
+            >
+              {submitError && <div className='error'>{submitError}</div>}
+              <Field
+                component={JInputField}
+                name='owner'
+                color='gray'
+                label={t`Current address`}
+                isDisabled
+              />
+              <Field
+                component={JInputField}
+                name='asset'
+                color='gray'
+                label={t`Asset address`}
+              />
+              <Field
+                component={JInputField}
+                name='recipient'
+                color='gray'
+                placeholder={t`Recipient address`}
+                infoMessage={this.getRecipientWarning(values)}
+              />
+              <Field
+                component={JInputField}
+                name='amount'
+                color='gray'
+                label={t`Amount`}
+              />
+              <JRaisedButton
+                onClick={handleSubmit}
+                isLoading={submitting}
+                color='blue'
+                label={t`Confirm`}
+                labelColor='white'
+                isWide
+              />
+            </div>
+          </form>
+        )}
+      />
+    )
+  }
 }
 
 export default FirstStepForm
