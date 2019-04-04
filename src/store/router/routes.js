ccd8ab7577c017775f5424ae38a70ff6b359e5c6
Ivan Violentov <violentoff@gmail.com>
2019-04-04 14:46:42 +0300

    Fix routes nesting

--- src/store/router/routes.js
+++ src/store/router/routes.js
@@ -1,4 +1,5 @@
 import createRouter5 from 'router5'
+
 import {
   selectActiveWallet,
   selectActiveWalletId,
@@ -9,120 +10,92 @@ export const router = createRouter5([], {
   allowNotFound: true,
 })
 
-export const routes = [
-  {
-    path: '/',
-    name: 'Home',
-  },
-  {
-    path: '/about',
-    name: 'About',
-  },
-  {
-    path: '/assets/add',
-    name: 'AssetsItemAdd',
-  },
-  {
-    path: '/assets',
-    name: 'AssetsManage',
-  },
-  {
-    path: '/assets/:assetId',
-    name: 'AssetsItem',
-  },
-  {
-    path: '/assets/:assetId/edit',
-    name: 'AssetsItemEdit',
-  },
-  {
-    path: '/contacts',
-    name: 'Contacts',
-  },
-  {
-    path: '/contacts/add',
-    name: 'ContactsItemAdd',
-  },
-  {
-    path: '/contacts/:contactId',
-    name: 'ContactsItemEdit',
-  },
-  {
-    path: '/history',
-    name: 'History',
-  },
-  {
-    path: '/history/:itemId',
-    name: 'HistoryItem',
-  },
-  {
-    path: '/more',
-    name: 'MoreActions',
-  },
-  {
-    path: '/receive',
-    name: 'Receive',
-  },
-  {
-    path: '/send',
-    name: 'Send',
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-  },
-  {
-    path: '/settings/currency',
-    name: 'SettingsCurrency',
-  },
-  {
-    path: '/settings/development',
-    name: 'SettingsDevelopment',
-  },
-  {
-    path: '/settings/language',
-    name: 'SettingsLanguage',
-  },
-  {
-    path: '/settings/security-password',
-    name: 'SettingsSecurityPassword',
-  },
-  {
-    path: '/support',
-    name: 'Support',
-  },
-  {
-    path: '/wallets',
-    name: 'Wallets',
-  },
-  {
-    path: '/wallets/create',
-    name: 'WalletsCreate',
-  },
-  {
-    path: '/wallets/import',
-    name: 'WalletsImport',
-  },
-  {
-    path: '/wallets/:walletId',
-    name: 'WalletsItem',
-  },
-  {
-    path: '/wallets/:walletId/backup',
-    name: 'WalletsItemBackup',
-  },
-  {
-    path: '/wallets/:walletId/delete',
-    name: 'WalletsItemDelete',
-  },
-  {
-    path: '/wallets/:walletId/rename',
-    name: 'WalletsItemRename',
-  },
-  {
-    path: '/wallets/:walletId/upgrade',
-    name: 'WalletsItemUpgrade',
-  },
-]
+export const routes = [{
+  path: '/',
+  name: 'Home',
+}, {
+  path: '/about',
+  name: 'About',
+}, {
+  path: '/assets/add',
+  name: 'AssetsItemAdd',
+}, {
+  path: '/assets',
+  name: 'AssetsManage',
+}, {
+  path: '/assets/:assetId',
+  name: 'AssetsItem',
+}, {
+  path: '/assets/:assetId/edit',
+  name: 'AssetsItemEdit',
+}, {
+  path: '/contacts',
+  name: 'Contacts',
+}, {
+  path: '/contacts/add',
+  name: 'ContactsItemAdd',
+}, {
+  path: '/contacts/:contactId',
+  name: 'ContactsItemEdit',
+}, {
+  path: '/history',
+  name: 'History',
+}, {
+  path: '/history/:itemId',
+  name: 'HistoryItem',
+}, {
+  path: '/more',
+  name: 'MoreActions',
+}, {
+  path: '/receive',
+  name: 'Receive',
+}, {
+  path: '/send',
+  name: 'Send',
+}, {
+  path: '/settings',
+  name: 'Settings',
+}, {
+  path: '/settings/currency',
+  name: 'SettingsCurrency',
+}, {
+  path: '/settings/development',
+  name: 'SettingsDevelopment',
+}, {
+  path: '/settings/language',
+  name: 'SettingsLanguage',
+}, {
+  path: '/settings/security-password',
+  name: 'SettingsSecurityPassword',
+}, {
+  path: '/support',
+  name: 'Support',
+}, {
+  path: '/wallets',
+  name: 'Wallets',
+}, {
+  path: '/wallets/create',
+  name: 'WalletsCreate',
+}, {
+  path: '/wallets/import',
+  name: 'WalletsImport',
+}, {
+  path: '/wallets/:walletId',
+  name: 'WalletsItem',
+}, {
+  path: '/wallets/:walletId/backup',
+  name: 'WalletsItemBackup',
+}, {
+  path: '/wallets/:walletId/delete',
+  name: 'WalletsItemDelete',
+}, {
+  path: '/wallets/:walletId/rename',
+  name: 'WalletsItemRename',
+}, {
+  path: '/wallets/:walletId/upgrade',
+  name: 'WalletsItemUpgrade',
+}]
+
 router.add(routes)
 
 // FIXME: this should be removed as soon as we get rid of
