a9bbcc5b4d0fe0bd960c6cf8512e78c2dc40379e
Ivan Violentov <violentoff@gmail.com>
2019-04-09 17:14:23 +0300

    Update util getErrorMessage

--- src/utils/form/getErrorMessage.js
+++ src/utils/form/getErrorMessage.js
@@ -4,11 +4,11 @@ export function getErrorMessage(
   meta: FinalFormMeta,
   validateType: ?FinalFormValidateType,
 ): ?string {
-  const error = meta.error || meta.submitError
-
-  const isDirty = (validateType === 'dirtySinceLastSubmit' && !meta[validateType])
-  const isTouched = (validateType === 'touched' && meta[validateType])
-  const isVisited = (validateType === 'visited' && meta[validateType])
+  const error: ?string = meta.error || meta.submitError
+  const hasValidateType: boolean = !!validateType && !!meta[validateType]
+  const isTouched: boolean  = (validateType === 'touched' && hasValidateType)
+  const isVisited: boolean  = (validateType === 'visited' && hasValidateType)
+  const isDirty: boolean  = (validateType === 'dirtySinceLastSubmit' && hasValidateType)
 
   if (isDirty) {
     return error
