ee4beac3f75a123caccaeeff75eebfbd681e0f5c
Michael Maslov <michael.maslov@jibrel.network>
2019-04-11 19:46:18 +0300

    Remove old module

--- src/pages/Send/components/index.js
+++ src/pages/Send/components/index.js
@@ -1,6 +1,5 @@
 // @flow
 
-export { default as HeaderPanel } from './HeaderPanel/HeaderPanel'
 export { default as FirstStepForm } from './FirstStepForm/FirstStepForm'
 export { default as PasswordStepForm } from './PasswordStepForm/PasswordStepFormContainer'
 export { default as ErrorMessage } from './ErrorMessage/ErrorMessage'
