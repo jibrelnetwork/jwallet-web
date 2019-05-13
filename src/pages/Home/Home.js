eb20c6ac9dd2ca179336f7992dd0ec83a43b1b28
Anton Yessaulenko <2114572+Creepypoke@users.noreply.github.com>
2019-05-13 13:40:20 +0300

    JWWEB-735 Move ETH on the top of assets list

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@
 
 import { connect } from 'react-redux'
 
-import isZero from 'utils/numbers/isZero'
 import { selectCurrentNetworkId } from 'store/selectors/networks'
 import { selectBalancesByBlockNumber } from 'store/selectors/balances'
 import { selectTransactionsByOwner } from 'store/selectors/transactions'
@@ -19,12 +18,11 @@ import {
 } from 'store/selectors/digitalAssets'
 
 import {
+  checkETH,
   searchDigitalAssets,
   filterAssetsBalances,
   flattenDigitalAssets,
-  compareDigitalAssetsByName,
   getDigitalAssetsWithBalance,
-  compareDigitalAssetsByBalance,
 } from 'utils/digitalAssets'
 
 import {
@@ -43,49 +41,6 @@ function filterActiveDigitalAssets(items: DigitalAssetWithBalance[]): DigitalAss
   return items.filter(({ isActive }: DigitalAssetWithBalance) => !!isActive)
 }
 
-function filterZeroBalanceDigitalAssets(
-  items: DigitalAssetWithBalance[],
-  isHideZeroBalance: boolean,
-): DigitalAssetWithBalance[] {
-  if (!isHideZeroBalance) {
-    return items
-  }
-
-  return items
-    .filter(({ balance }: DigitalAssetWithBalance): boolean => !!balance && !isZero(balance.value))
-}
-
-function sortDigitalAssets(
-  items: DigitalAssetWithBalance[],
-  filterOptions: DigitalAssetsFilterOptions,
-): DigitalAssetWithBalance[] {
-  const {
-    sortBy,
-    sortByNameDirection,
-    sortByBalanceDirection,
-  }: DigitalAssetsFilterOptions = filterOptions
-
-  // eslint-disable-next-line fp/no-mutating-methods
-  const itemsSortedByName: DigitalAssetWithBalance[] = [...items].sort((
-    first: DigitalAssetWithBalance,
-    second: DigitalAssetWithBalance,
-  ): number => compareDigitalAssetsByName(first.name, second.name, sortByNameDirection))
-
-  if (sortBy === 'balance') {
-    // eslint-disable-next-line fp/no-mutating-methods
-    return [...itemsSortedByName].sort((
-      first: DigitalAssetWithBalance,
-      second: DigitalAssetWithBalance,
-    ): number => compareDigitalAssetsByBalance(
-      first,
-      second,
-      sortByBalanceDirection,
-    ))
-  }
-
-  return itemsSortedByName
-}
-
 function prepareDigitalAssets(
   items: DigitalAssetWithBalance[],
   filterOptions: DigitalAssetsFilterOptions,
@@ -93,22 +48,15 @@ function prepareDigitalAssets(
 ): DigitalAssetWithBalance[] {
   const itemsActive: DigitalAssetWithBalance[] = filterActiveDigitalAssets(items)
 
-  const itemsFiltered: DigitalAssetWithBalance[] = filterZeroBalanceDigitalAssets(
-    itemsActive,
-    filterOptions.isHideZeroBalance,
-  )
-
   const itemsFound: DigitalAssetWithBalance[] = searchDigitalAssets(
-    itemsFiltered,
+    itemsActive,
     searchQuery,
   )
 
-  const itemsSorted: DigitalAssetWithBalance[] = sortDigitalAssets(
-    itemsFound,
-    filterOptions,
-  )
-
-  return itemsSorted
+  return itemsFound.reduce((reduceResult, item) =>
+    checkETH(item.blockchainParams.address)
+      ? [item, ...reduceResult]
+      : [...reduceResult, item], [])
 }
 
 function mapStateToProps(state: AppState) {
