1316242592b6f5a4e78716860b41a92c4616ecc6
Michael Maslov <michael.maslov@jibrel.network>
2019-05-23 19:24:35 +0300

    Refactor JSwitch

--- src/components/base/JSwitch/JSwitch.stories.js
+++ src/components/base/JSwitch/JSwitch.stories.js
@@ -1,16 +1,53 @@
 // @flow
 
 import React from 'react'
+import { withState } from 'recompose'
 import { storiesOf } from '@storybook/react'
 
 import { JSwitch } from 'components/base'
 
+const StateHOC = withState('value', 'setValue', '')
+
+// $FlowFixMe
 storiesOf('JSwitch')
   .add('Default', () => (
     <div className='story'>
-      <h2>Default unchecked</h2>
-      <JSwitch name='Test' />
-      <h2>Default checked</h2>
-      <JSwitch name='Test-2' isChecked />
+      {React.createElement(StateHOC(
+        ({
+          value, setValue,
+        }) => (
+          <JSwitch
+            name='Test'
+            onChange={setValue}
+            isChecked={value}
+          />
+        ),
+      ))}
+    </div>
+  ))
+  .add('Disabled', () => (
+    <div className='story'>
+      {React.createElement(StateHOC(
+        ({
+          value, setValue,
+        }) => (
+          <JSwitch
+            name='Test'
+            isDisabled
+            onChange={setValue}
+            isChecked={value}
+          />
+        ),
+      ))}
+    </div>
+  ))
+  .add('Disabled + checked', () => (
+    <div className='story'>
+      <JSwitch
+        name='Test'
+        onChange={() => null}
+        isDisabled
+        isChecked
+      />
     </div>
   ))
