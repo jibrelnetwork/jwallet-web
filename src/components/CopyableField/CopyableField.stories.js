d7ea1c74855d3236f654b6f35c864cf3b559af16
Ivan Violentov <violentoff@gmail.com>
2019-05-30 18:55:15 +0300

    Update CopyableField component

--- src/components/CopyableField/CopyableField.stories.js
+++ src/components/CopyableField/CopyableField.stories.js
@@ -1,27 +1,16 @@
-// @flow
+// @flow strict
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import CopyableField from 'components/CopyableField'
+import { CopyableField } from './CopyableField'
 
-const HANDLER = console.log
-const PRIVATE_KEY = '0xfb27c2394586feb01403ba3643b519c8f209e0427b31f510a6877b494c020f59'
 const MNEMONIC = 'extra write absent bargain raw tilt follow jeans february brief nation express'
 
-storiesOf('CopyableField')
-  .add('only onCopySuccess handler (mnemonic)', () => (
+storiesOf('CopyableField', module)
+  .add('default', () => (
     <CopyableField
-      onCopySuccess={HANDLER}
       value={MNEMONIC}
-    />
-  ))
-  .add('onCopySuccess & onDownloadSuccess handlers (private key)', () => (
-    <CopyableField
-      onCopySuccess={HANDLER}
-      onDownloadSuccess={HANDLER}
-      value={PRIVATE_KEY}
-      valueToDisplay={`${PRIVATE_KEY.substr(0, 33)} ${PRIVATE_KEY.substr(33)}`}
-      isDownloadAvailable
+      label='Mnemonic Phrase'
     />
   ))
