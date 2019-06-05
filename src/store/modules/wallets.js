9e805e70f6918309a0dd758ea384d3bc32b7692b
Ivan Violentov <violentoff@gmail.com>
2019-06-05 12:48:07 +0300

    Remove unused wallets modules

--- src/store/modules/wallets.js
+++ src/store/modules/wallets.js
@@ -1,17 +1,7 @@
 // @flow strict
 
-export type WalletsPrivateKeyRequestPayload = {|
-  +wallet: Wallet,
-  +internalKey: ?EncryptedData,
-  +password: string,
-|}
-
 export const SET_WALLETS_ITEMS = '@@wallets/SET_WALLETS_ITEMS'
 export const SET_ACTIVE_WALLET = '@@wallets/SET_ACTIVE_WALLET'
-
-export const PRIVATE_KEY_ERROR = '@@wallets/PRIVATE_KEY_ERROR'
-export const PRIVATE_KEY_SUCCESS = '@@wallets/PRIVATE_KEY_SUCCESS'
-export const PRIVATE_KEY_REQUEST = '@@wallets/PRIVATE_KEY_REQUEST'
 
 export const SIMPLIFY_WALLET = '@@wallets/SIMPLIFY_WALLET'
 
@@ -33,33 +23,6 @@ export function setActiveWallet(activeWalletId: ?WalletId) {
   }
 }
 
-export function privateKeyError(walletId: string, message: string) {
-  return {
-    type: PRIVATE_KEY_ERROR,
-    payload: {
-      walletId,
-      message,
-    },
-  }
-}
-
-export function privateKeySuccess(walletId: string, privateKey: string) {
-  return {
-    type: PRIVATE_KEY_SUCCESS,
-    payload: {
-      walletId,
-      privateKey,
-    },
-  }
-}
-
-export function privateKeyRequest(payload: WalletsPrivateKeyRequestPayload) {
-  return {
-    type: PRIVATE_KEY_REQUEST,
-    payload,
-  }
-}
-
 export function simplifyWallet(walletId: WalletId, isSimplified: boolean) {
   return {
     type: SIMPLIFY_WALLET,
@@ -73,9 +36,6 @@ export function simplifyWallet(walletId: WalletId, isSimplified: boolean) {
 export type WalletsAction =
   ExtractReturn<typeof setWalletsItems> |
   ExtractReturn<typeof setActiveWallet> |
-  ExtractReturn<typeof privateKeyError> |
-  ExtractReturn<typeof privateKeySuccess> |
-  ExtractReturn<typeof privateKeyRequest> |
   ExtractReturn<typeof simplifyWallet>
 
 const initialState: WalletsState = {
