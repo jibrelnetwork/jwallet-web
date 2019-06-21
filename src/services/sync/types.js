bd427630ad800014ea8dbf129006580e45d6c106
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-06-21 15:50:27 +0300

    Add types for IndexedDB Transactions and Blocks items storage

---
+++ src/services/sync/types.js
@@ -0,0 +1,34 @@
+// @flow strict
+
+export type DBTransaction = {
+  id: string, // primary key
+  assetAddress: string, // index
+  timestamp: number, // index
+  type: string, // index
+    // transferIn
+    // transferOut
+    // mint
+    // burn
+    // contractCall
+    // ...
+  blockNumber: number, // index
+  blockHash: string,
+  from: string,
+  to: string,
+  amount: string, // in decimal format
+  hash: string, // parent tx hash for events
+  gas: string, // in decimal format, parent value for events
+  gasUsed: string, // in decimal format
+  gasPrice: string, // in decimal format, parent value for events
+  status: number, // 1 for success, 0 for fail
+  nonce: number, // parent value for events
+}
+
+export type DBBlock = {
+  id: number, // primary key, block number for now
+  number: number,
+  hash: string,
+  parentHash: string,
+  timestamp: number,
+  transactions: string[],
+}
