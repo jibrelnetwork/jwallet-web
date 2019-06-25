3e1dc9aea588b0a30fb5e4cc9bf39b6f57728e8a
Ivan Violentov <violentoff@gmail.com>
2019-06-25 16:40:30 +0300

    Update WalletsView

--- src/pages/Wallets/WalletsView.js
+++ src/pages/Wallets/WalletsView.js
@@ -1,10 +1,23 @@
 // @flow strict
 
 import React from 'react'
+import classNames from 'classnames'
+import { t } from 'ttag'
 
-import { WalletCard } from 'components'
+import buttonStyle from 'components/base/Button/button.m.scss'
+import titleHeaderStyle from 'components/TitleHeader/titleHeader.m.scss'
 
-import walletsStyles from './wallets.m.scss'
+import {
+  WalletCard,
+  TitleHeader,
+} from 'components'
+
+import {
+  JIcon,
+  JLink,
+} from 'components/base'
+
+import style from './wallets.m.scss'
 
 export type Props = {|
   +items: Wallets,
@@ -16,9 +29,35 @@ export function WalletsView({
   activeWalletId,
 }: Props) {
   return (
-    <div className={walletsStyles.core}>
-      <div className={walletsStyles.content}>
-        <div className={walletsStyles.wallets}>
+    <div className={style.core}>
+      <TitleHeader title='My Wallets'>
+        <JLink
+          className={classNames(buttonStyle.additionalIcon, titleHeaderStyle.action)}
+          href='/wallets/create'
+        >
+          <JIcon
+            name='ic_add_24-use-fill'
+            className={titleHeaderStyle.icon}
+          />
+          <span className={titleHeaderStyle.label}>
+            {t`Create Wallet`}
+          </span>
+        </JLink>
+        <JLink
+          className={classNames(buttonStyle.additionalIcon, titleHeaderStyle.action)}
+          href='/wallets/import'
+        >
+          <JIcon
+            name='ic_import_wallet_24-use-fill'
+            className={titleHeaderStyle.icon}
+          />
+          <span className={titleHeaderStyle.label}>
+            {t`Import Wallet`}
+          </span>
+        </JLink>
+      </TitleHeader>
+      <div className={style.content}>
+        <div className={style.wallets}>
           {items.map(({ id }: Wallet) => (
             <WalletCard
               id={id}
