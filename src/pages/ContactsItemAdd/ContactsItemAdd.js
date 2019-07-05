a72bb0886448281580a8971cf0e8f9707eeb6c7d
Michael Maslov <michael.maslov@jibrel.network>
2019-07-05 23:02:19 +0300

    WP: contacts item add

--- src/pages/ContactsItemAdd/ContactsItemAdd.js
+++ src/pages/ContactsItemAdd/ContactsItemAdd.js
@@ -1,17 +1,39 @@
 // @flow strict
 
+import React from 'react'
 import { connect } from 'react-redux'
+import { t } from 'ttag'
+import { actions as routerActions } from 'redux-router5'
 
 import {
-  type Props,
-  ContactsItemAddView,
-} from './ContactsItemAddView'
+  TitleHeader,
+  ContactsEditForm,
+} from 'components'
+import * as contacts from 'store/modules/contacts'
+
+import style from './contactsItemAdd.m.scss'
 
 type OwnProps = {|
   +address?: ContactId,
   +name?: string,
   +note?: string,
 |}
+
+type Props = {|
+  ...OwnProps,
+  saveContact: (contact: Contact) => Promise<*>,
+|}
+
+function ContactsItemAddView(props: Props) {
+  return (
+    <div className={style.core}>
+      <TitleHeader
+        title={t`Add Contact`}
+      />
+      <ContactsEditForm {...props} />
+    </div>
+  )
+}
 
 function mapStateToProps(
   state: AppState,
@@ -23,7 +45,7 @@ function mapStateToProps(
 ) {
   const handledAddress = address || ''
   const handledNote = note || ''
-  const handledName = name || 'Sara Jessica Parcel'
+  const handledName = name || ''
 
   return {
     address: handledAddress,
@@ -32,5 +54,12 @@ function mapStateToProps(
   }
 }
 
-export const ContactsItemAdd =
-  connect< Props, OwnProps, _, _, _, _ >(mapStateToProps)(ContactsItemAddView)
+const mapDispatchToProps = {
+  saveContact: contacts.update,
+  goBack: () => routerActions.navigateTo('Contacts'),
+}
+
+export const ContactsItemAdd = connect< Props, OwnProps, _, _, _, _ >(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ContactsItemAddView)
