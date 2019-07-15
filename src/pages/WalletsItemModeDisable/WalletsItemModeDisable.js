6453c7699ea9638594fac36b9b1a36bbb2e8e676
Ivan Violentov <violentoff@gmail.com>
2019-07-15 16:47:52 +0300

    Update WalletsItemModeDisable page (integrate AddressPicker)

--- src/pages/WalletsItemModeDisable/WalletsItemModeDisable.js
+++ src/pages/WalletsItemModeDisable/WalletsItemModeDisable.js
@@ -1,43 +1,24 @@
 // @flow strict
 
-import React, { PureComponent } from 'react'
-import { i18n } from 'i18n/lingui'
+import { connect } from 'react-redux'
 
-import { Button } from 'components/base'
-import { UserActionInfo } from 'components'
-import { walletsPlugin } from 'store/plugins'
+import { selectAddressNames } from 'store/selectors/wallets'
 
-import walletsItemModeDisableStyle from './walletsItemModeDisable.m.scss'
+import {
+  type Props,
+  WalletsItemModeDisableView,
+} from './WalletsItemModeDisableView'
 
-export type Props = {|
+type OwnProps = {|
   +walletId: string,
 |}
 
-export class WalletsItemModeDisable extends PureComponent<Props> {
-  handleDisable = () => {
-    walletsPlugin.switchMode(this.props.walletId)
-  }
-
-  render() {
-    /* eslint-disable max-len */
-    return (
-      <div className={walletsItemModeDisableStyle.core}>
-        <UserActionInfo
-          text={i18n._('WalletsItemModeDisable.description', null, { defaults: 'This action will leave only one active wallet address of your choice. \nYou will be able return to the multi-address mode at any time and get access to all \nyour currently available addresses.' })}
-          title={i18n._('WalletsItemModeDisable.title', null, { defaults: 'Disable Multi-Address Mode' })}
-          iconClassName={walletsItemModeDisableStyle.icon}
-          iconName='ic_attention_48-use-fill'
-        />
-        <div className={walletsItemModeDisableStyle.buttons}>
-          <Button
-            onClick={this.handleDisable}
-            theme='general'
-          >
-            {i18n._('WalletsItemModeDisable.actions.submit', null, { defaults: 'Disable' })}
-          </Button>
-        </div>
-      </div>
-    )
-    /* eslint-enable max-len */
+function mapStateToProps(state: AppState) {
+  return {
+    addressNames: selectAddressNames(state),
   }
 }
+
+export const WalletsItemModeDisable = connect<Props, OwnProps, _, _, _, _>(
+  mapStateToProps,
+)(WalletsItemModeDisableView)
