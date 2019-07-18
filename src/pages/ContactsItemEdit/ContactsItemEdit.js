467b959e4032e814ce26af97c6cc53e0963443be
Michael Maslov <michael.maslov@jibrel.network>
2019-07-18 20:05:35 +0300

    JWWEB-875 - Edit contact

--- src/pages/ContactsItemEdit/ContactsItemEdit.js
+++ src/pages/ContactsItemEdit/ContactsItemEdit.js
@@ -1,26 +1,103 @@
 // @flow strict
 
+import React from 'react'
 import { connect } from 'react-redux'
+import { actions as routerActions } from 'redux-router5'
+import { useI18n } from 'app/hooks'
+
+import { TitleHeader } from 'components'
+import { selectFavorite } from 'store/selectors/favorites'
+
+import * as favorites from 'store/modules/favorites'
 
 import {
-  type Props,
-  ContactsItemEditView,
-} from './ContactsItemEditView'
+  ContactEditForm,
+  type FormValues,
+} from './components/ContactEditForm/ContactEditForm'
+
+import style from './contactsItemEdit.m.scss'
 
 type OwnProps = {|
-  +contactId: ContactId,
+  +contactId?: OwnerAddress,
 |}
 
-function mapStateToProps(state: AppState, { contactId }: OwnProps) {
-  const note = ''
-  const name = contactId
+type Props = {|
+  ...OwnProps,
+  +name: string,
+  +description: string,
+  +address: OwnerAddress,
+  +checkContactExists: (address: OwnerAddress) => boolean,
+  +editContact: (contact: FormValues) => ?FormValues,
+  +onDeleteClick: (address: OwnerAddress) => any,
+  +goBack: () => any,
+|}
+
+function ContactsItemEditView({
+  address,
+  name,
+  description,
+  checkContactExists,
+  editContact,
+  onDeleteClick,
+  goBack,
+}: Props) {
+  const i18n = useI18n()
+
+  const initialValues = {
+    name,
+    address,
+    description,
+  }
+
+  return (
+    <div className={style.core}>
+      <TitleHeader
+        title={i18n._(
+          'ContactsItemEdit.title',
+          null,
+          { defaults: 'Edit contact' },
+        )}
+      />
+      <ContactEditForm
+        initialValues={initialValues}
+        checkContactExists={checkContactExists}
+        editContact={editContact}
+        onDeleteClick={onDeleteClick}
+        goBack={goBack}
+      />
+    </div>
+  )
+}
+
+const checkContactExists = (state: AppState) => (address: OwnerAddress) =>
+  !!selectFavorite(state, address)
+
+function mapStateToProps(
+  state: AppState,
+  { contactId }: OwnProps,
+) {
+  const {
+    address,
+    name = '',
+    description = '',
+  } = selectFavorite(state, contactId || '') || {}
 
   return {
-    contactId,
-    note,
+    address,
     name,
+    description,
+    checkContactExists: checkContactExists(state),
   }
 }
 
-export const ContactsItemEdit =
-  connect< Props, OwnProps, _, _, _, _ >(mapStateToProps)(ContactsItemEditView)
+const mapDispatchToProps = {
+  editContact: favorites.update,
+  goBack: () => routerActions.navigateTo('Contacts'),
+  onDeleteClick: (address: OwnerAddress) =>
+    routerActions.navigateTo('ContactsItemDelete', { contactId: address }),
+}
+
+export const ContactsItemEdit = connect<Props, OwnProps, _, _, _, _ >(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ContactsItemEditView)
---
+++ src/pages/ContactsItemEdit/components/ContactEditForm/ContactEditForm.js
@@ -0,0 +1,136 @@
+// @flow strict
+
+import React, { PureComponent } from 'react'
+import { withI18n } from '@lingui/react'
+import { type I18n as I18nType } from '@lingui/core'
+
+import {
+  Form,
+  Field,
+  type FormRenderProps,
+} from 'react-final-form'
+
+import {
+  Button,
+  JInputField,
+} from 'components/base'
+
+import offset from 'styles/offsets.m.scss'
+import style from './contactEditForm.m.scss'
+
+export type FormValues = {|
+  address: string,
+  description: string, // note
+  name: string,
+|}
+
+export type Props = {|
+  +initialValues: FormValues,
+  +i18n: I18nType,
+  +checkContactExists: (address: OwnerAddress) => boolean,
+  +editContact: (contact: FormValues) => any,
+  +onDeleteClick: (address: OwnerAddress) => any,
+  +goBack: () => any,
+|}
+
+class ContactEditFormComponent extends PureComponent<Props> {
+  handleSubmit = (values: FormValues) => {
+    this.props.editContact(values)
+    this.props.goBack()
+  }
+
+  componentDidMount() {
+    const {
+      initialValues: {
+        address,
+      },
+      goBack,
+      checkContactExists,
+    } = this.props
+
+    if (!checkContactExists(address)) {
+      goBack()
+    }
+  }
+
+  handleDeleteClick = () => {
+    this.props.onDeleteClick(this.props.initialValues.address)
+  }
+
+  render() {
+    const {
+      i18n,
+      initialValues,
+    } = this.props
+
+    return (
+      <Form
+        render={({
+          handleSubmit,
+          submitting,
+        }: FormRenderProps) => (
+          <form
+            onSubmit={handleSubmit}
+            className={`${style.form} ${offset.mb16}`}
+          >
+            <Field
+              component={JInputField}
+              label={i18n._(
+                'ContactForm.input.name.title',
+                null,
+                { defaults: 'Name' },
+              )}
+              name='name'
+              isDisabled={submitting}
+            />
+            <Field
+              component={JInputField}
+              label={i18n._(
+                'ContactForm.input.address.title',
+                null,
+                { defaults: 'Address' },
+              )}
+              name='address'
+              isDisabled
+            />
+            <Field
+              className={offset.mb32}
+              component={JInputField}
+              label={i18n._(
+                'ContactForm.input.note.title',
+                null,
+                { defaults: 'Note' },
+              )}
+              name='description'
+              infoMessage={i18n._(
+                'ContactForm.input.note.info',
+                null,
+                { defaults: 'This note is only visible to you.' },
+              )}
+              isDisabled={submitting}
+            />
+            <Button
+              type='submit'
+              className={offset.mb16}
+              isLoading={submitting}
+            >
+              {i18n._('ContactForm.actions.save', null, { defaults: 'Save' })}
+            </Button>
+            <Button
+              type='button'
+              isLoading={submitting}
+              theme='secondary'
+              onClick={this.handleDeleteClick}
+            >
+              {i18n._('ContactForm.actions.delete', null, { defaults: 'Delete Contact' })}
+            </Button>
+          </form>
+        )}
+        onSubmit={this.handleSubmit}
+        initialValues={initialValues}
+      />
+    )
+  }
+}
+
+export const ContactEditForm = withI18n()(ContactEditFormComponent)
