bb1844c4a1c85d13317b556b4ab26442b22dfd57
Michael Maslov <michael.maslov@jibrel.network>
2019-07-18 17:40:41 +0300

    Implement contacts add

--- src/pages/ContactsItemAdd/ContactsItemAdd.js
+++ src/pages/ContactsItemAdd/ContactsItemAdd.js
@@ -2,64 +2,89 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { t } from 'ttag'
 import { actions as routerActions } from 'redux-router5'
+import { useI18n } from 'app/hooks'
+
+import { TitleHeader } from 'components'
+import { selectFavorite } from 'store/selectors/favorites'
+
+import * as favorites from 'store/modules/favorites'
 
 import {
-  TitleHeader,
-  ContactsEditForm,
-} from 'components'
-import * as contacts from 'store/modules/contacts'
+  ContactAddForm,
+  type FormValues,
+} from './components/ContactAddForm/ContactAddForm'
 
 import style from './contactsItemAdd.m.scss'
 
 type OwnProps = {|
-  +address?: ContactId,
+  +address?: OwnerAddress,
   +name?: string,
-  +note?: string,
 |}
 
 type Props = {|
   ...OwnProps,
-  saveContact: (contact: Contact) => Promise<*>,
+  +checkContactExists: (address: OwnerAddress) => boolean,
+  +addContact: (contact: FormValues) => ?FormValues,
+  +goBack: () => any,
 |}
 
-function ContactsItemAddView(props: Props) {
+function ContactsItemAddView({
+  address,
+  name,
+  checkContactExists,
+  addContact,
+  goBack,
+}: Props) {
+  const i18n = useI18n()
+
+  const initialValues = {
+    name,
+    address,
+  }
+
   return (
     <div className={style.core}>
       <TitleHeader
-        title={t`Add Contact`}
+        title={i18n._(
+          'ContactsItemAdd.title',
+          null,
+          { defaults: 'Add contact' },
+        )}
       />
-      <ContactsEditForm {...props} />
+      <ContactAddForm
+        initialValues={initialValues}
+        checkContactExists={checkContactExists}
+        addContact={addContact}
+        goBack={goBack}
+      />
     </div>
   )
 }
+
+const checkContactExists = (state: AppState) => (address: OwnerAddress) =>
+  !!selectFavorite(state, address)
 
 function mapStateToProps(
   state: AppState,
   {
     address,
     name,
-    note,
   }: OwnProps,
 ) {
-  const handledAddress = address || ''
-  const handledNote = note || ''
-  const handledName = name || ''
-
   return {
-    address: handledAddress,
-    note: handledNote,
-    name: handledName,
+    address: address || '',
+    name: name || '',
+    checkContactExists: checkContactExists(state),
   }
 }
 
 const mapDispatchToProps = {
-  saveContact: contacts.update,
+  addContact: favorites.add,
   goBack: () => routerActions.navigateTo('Contacts'),
 }
 
-export const ContactsItemAdd = connect< Props, OwnProps, _, _, _, _ >(
+export const ContactsItemAdd = connect<Props, OwnProps, _, _, _, _ >(
   mapStateToProps,
   mapDispatchToProps,
 )(ContactsItemAddView)
