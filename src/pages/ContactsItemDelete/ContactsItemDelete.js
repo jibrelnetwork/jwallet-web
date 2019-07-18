a0978f05743a288acac8f2b68f5fc4011bea5d9d
Michael Maslov <michael.maslov@jibrel.network>
2019-07-18 20:45:14 +0300

    JWWEB-877 - Delete contact

--- src/pages/ContactsItemDelete/ContactsItemDelete.js
+++ src/pages/ContactsItemDelete/ContactsItemDelete.js
@@ -1,6 +1,11 @@
 // @flow strict
 
 import { connect } from 'react-redux'
+import { actions as routerActions } from 'redux-router5'
+
+import { selectFavorite } from 'store/selectors/favorites'
+
+import * as favorites from 'store/modules/favorites'
 
 import {
   type Props,
@@ -8,17 +13,27 @@ import {
 } from './ContactsItemDeleteView'
 
 type OwnProps = {|
-  +contactId: ContactId,
+  +contactId: OwnerAddress,
 |}
 
 function mapStateToProps(state: AppState, { contactId }: OwnProps) {
-  const name = 'Sarah Jessica Parcel'
+  const {
+    address,
+    name = '',
+  } = selectFavorite(state, contactId || '') || {}
 
   return {
-    contactId,
+    address,
     name,
   }
 }
 
-export const ContactsItemDelete =
-  connect< Props, OwnProps, _, _, _, _ >(mapStateToProps)(ContactsItemDeleteView)
+const mapDispatchToProps = {
+  onDeleteContact: (address: OwnerAddress) => favorites.remove(address),
+  goBack: () => routerActions.navigateTo('Contacts'),
+}
+
+export const ContactsItemDelete = connect<Props, OwnProps, _, _, _, _>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ContactsItemDeleteView)
