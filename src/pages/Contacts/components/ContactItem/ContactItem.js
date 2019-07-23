2c542315d78cea86af89762efcaf1eaa6d25beaf
Michael Maslov <michael.maslov@jibrel.network>
2019-07-23 18:31:47 +0300

    JWWEB-920 - Implement contact list

--- src/pages/Contacts/components/ContactItem/ContactItem.js
+++ src/pages/Contacts/components/ContactItem/ContactItem.js
@@ -2,38 +2,35 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { keyBy } from 'lodash-es'
 
 import {
-  ItemCard, JIcon,
+  ItemCard,
+  JIcon,
 } from 'components/base'
 import { formatInitials } from 'utils/formatters'
+import { selectFavorite } from 'store/selectors/favorites'
 
 import offset from 'styles/offsets.m.scss'
-
-import CONTACT_LIST from 'pages/Contacts/CONTACT_LIST'
-
 import style from './contactItem.m.scss'
 
 type OwnProps = {|
-  +contactId: ContactId,
+  +address: OwnerAddress,
 |}
 
 type Props = {|
   +onClick: Function,
-  +id: ContactId,
+  +address: OwnerAddress,
   +name: string,
-  +note: string,
+  +description: string,
   +isActive?: boolean,
-  ...OwnProps,
 |}
 
-function Component(props: Props) {
+function ContactItemComponent(props: Props) {
   const nameInitials = formatInitials(props.name)
 
   return (
     <ItemCard
-      href={`/contacts/${props.id}`}
+      href={`/contacts/${props.address}`}
       onClick={props.onClick}
       isActive={props.isActive}
       className={`__contact-item ${offset.mb16}`}
@@ -47,20 +44,27 @@ function Component(props: Props) {
         </div>
       </div>
       <div className={`${style.item} ${style.mainBlock}`}>
-        <div className={style.text}>{props.name || props.id}</div>
-        <div className={style.subtext}>{props.note}</div>
+        <div className={style.text}>{props.name || props.address}</div>
+        <div className={style.subtext}>{props.description}</div>
       </div>
     </ItemCard>
   )
 }
 
-Component.defaultProps = {
+ContactItemComponent.defaultProps = {
   isActive: false,
 }
 
-export const ContactItem =
-  connect< Props, OwnProps, _, _, _, _ >((state: AppState, { contactId }: OwnProps) => {
-    const contact = keyBy(CONTACT_LIST, 'id')[contactId]
+function mapStateToProps(state: AppState, ownProps: OwnProps) {
+  const { address } = ownProps
 
-    return contact
-  })(React.memo<Props>(Component))
+  const contact = selectFavorite(state, address)
+
+  return {
+    ...contact,
+  }
+}
+
+export const ContactItem = connect< Props, OwnProps, _, _, _, _ >(
+  mapStateToProps,
+)(ContactItemComponent)
