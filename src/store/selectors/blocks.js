29143240292ef6b1286ca5201b74578500fcd0ec
Ivan Violentov <violentoff@gmail.com>
2019-07-25 12:36:38 +0300

    Update blocks selectors

--- src/store/selectors/blocks.js
+++ src/store/selectors/blocks.js
@@ -1,4 +1,6 @@
-// @flow
+// @flow strict
+
+import { selectCurrentNetworkId } from 'store/selectors/networks'
 
 export function selectBlocks(state: AppState): BlocksState {
   return state.blocks
@@ -16,25 +18,41 @@ export function selectBlocksItems(state: AppState): BlocksByNetworkId {
   return blocksPersist.items
 }
 
-export function selectBlocksByNetworkId(state: AppState, networkId: NetworkId): ?Blocks {
+export function selectBlocksByNetworkId(
+  state: AppState,
+  networkIdOptional?: NetworkId,
+): ?Blocks {
+  const networkId: NetworkId = networkIdOptional || selectCurrentNetworkId(state)
   const blocksItems: BlocksByNetworkId = selectBlocksItems(state)
 
   return blocksItems[networkId]
 }
 
-export function selectLatestBlock(state: AppState, networkId: NetworkId): ?BlockData {
+export function selectLatestBlock(
+  state: AppState,
+  networkIdOptional?: NetworkId,
+): ?BlockData {
+  const networkId: NetworkId = networkIdOptional || selectCurrentNetworkId(state)
   const blocksByNetworkId: ?Blocks = selectBlocksByNetworkId(state, networkId)
 
   return blocksByNetworkId ? blocksByNetworkId.latest : null
 }
 
-export function selectCurrentBlock(state: AppState, networkId: NetworkId): ?BlockData {
+export function selectCurrentBlock(
+  state: AppState,
+  networkIdOptional?: NetworkId,
+): ?BlockData {
+  const networkId: NetworkId = networkIdOptional || selectCurrentNetworkId(state)
   const blocksByNetworkId: ?Blocks = selectBlocksByNetworkId(state, networkId)
 
   return blocksByNetworkId ? blocksByNetworkId.current : null
 }
 
-export function selectProcessingBlock(state: AppState, networkId: NetworkId): ?BlockData {
+export function selectProcessingBlock(
+  state: AppState,
+  networkIdOptional?: NetworkId,
+): ?BlockData {
+  const networkId: NetworkId = networkIdOptional || selectCurrentNetworkId(state)
   const blocksByNetworkId: ?Blocks = selectBlocksByNetworkId(state, networkId)
 
   return blocksByNetworkId ? blocksByNetworkId.processing : null
