0ba43a700fd0f68b4bfd0fb3cd63eb64bd4395a6
Michael Maslov <michael.maslov@jibrel.network>
2019-07-30 17:48:15 +0300

    Delete legacy

--- src/store/sagas/digitalAssets/index.js
+++ src/store/sagas/digitalAssets/index.js
@@ -3,11 +3,9 @@
 import { digitalAssetsRootSaga } from './core'
 import { digitalAssetsAddRootSaga } from './add'
 import { digitalAssetsEditRootSaga } from './edit'
-import { digitalAssetsGridRootSaga } from './grid'
 
 export default [
   digitalAssetsRootSaga,
   digitalAssetsAddRootSaga,
   digitalAssetsEditRootSaga,
-  digitalAssetsGridRootSaga,
 ]
