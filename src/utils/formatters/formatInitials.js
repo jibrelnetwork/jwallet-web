6ad5bf02e1458d0b2da1757411fd9efc484973cb
Michael Maslov <michael.maslov@jibrel.network>
2019-07-30 19:33:07 +0300

    Fix formtInitials

--- src/utils/formatters/formatInitials.js
+++ src/utils/formatters/formatInitials.js
@@ -9,8 +9,8 @@ export function formatInitials(name: string): ?string {
     case 0:
       return null
     case 1:
-      return dividedName[0].firstChar
+      return `${dividedName[0].firstChar}${dividedName[0].secondChar}`.toUpperCase()
     default:
-      return `${dividedName[0].firstChar}${dividedName[1].firstChar}`
+      return `${dividedName[0].firstChar}${dividedName[1].firstChar}`.toUpperCase()
   }
 }
