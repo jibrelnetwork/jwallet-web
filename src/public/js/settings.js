865c6ff275cb8c3df78539cb7bbc293a9dd597d8
Ivan Violentov <violentoff@gmail.com>
2019-08-28 15:57:26 +0300

    Add default values for ticker&blockexplorer api

--- src/public/js/settings.js
+++ src/public/js/settings.js
@@ -4,5 +4,5 @@ window.__MAIN_RPC_PORT__ = '{{ default .Env.MAIN_RPC_PORT "443" }}'
 window.__ROPSTEN_RPC_ADDR__ = '{{ default .Env.ROPSTEN_RPC_ADDR "ropsten.jnode.network" }}'
 window.__ROPSTEN_RPC_PORT__ = '{{ default .Env.ROPSTEN_RPC_PORT "443" }}'
 
-window.__TICKER_API__ = '{{ default .Env.TICKER_API "" }}'
-window.__BLOCKEXPLORER_API__ = '{{ default .Env.BLOCKEXPLORER_API "" }}'
+window.__TICKER_API__ = '{{ default .Env.TICKER_API "https://api-fiat-courses.jwallet.network" }}'
+window.__BLOCKEXPLORER_API__ = '{{ default .Env.BLOCKEXPLORER_API "https://api-blockchain-service.jwallet.network" }}'
