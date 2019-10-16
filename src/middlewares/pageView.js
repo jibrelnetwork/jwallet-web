f8aeacad1888003dc554e1f71b2e35c0da99bbc9
Ivan Violentov <violentoff@gmail.com>
2019-10-16 18:50:28 +0300

    Add pageView middleware

---
+++ src/middlewares/pageView.js
@@ -0,0 +1,29 @@
+// @flow strict
+
+import {
+  gaSendPageView,
+  gaSendException,
+} from 'utils/analytics'
+
+const ROUTER5_PAGE_VIEW_ACTION_TYPE = '@@router5/TRANSITION_SUCCESS'
+
+export function pageView() {
+  return (next: Function) => (action: any) => {
+    try {
+      if (action.type === ROUTER5_PAGE_VIEW_ACTION_TYPE) {
+        console.error(action.payload.route.path)
+
+        gaSendPageView(action.payload.route.path)
+      }
+    } catch (error) {
+      console.error(error)
+
+      gaSendException({
+        exDescription: `Error sending page view: ${error.message}`,
+        exFatal: false,
+      })
+    }
+
+    next(action)
+  }
+}
