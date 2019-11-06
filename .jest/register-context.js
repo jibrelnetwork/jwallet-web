bba2db955ad97e22d9732734aa200645cf005da3
Ivan Violentov <violentoff@gmail.com>
2019-11-06 13:27:50 +0300

    Add 'i18n/lingui' mock

--- .jest/register-context.js
+++ .jest/register-context.js
@@ -14,3 +14,34 @@ jest.mock('../src/utils/sprite/coloredIconsUI', () => ({
 }))
 
 jest.mock('../src/public/assets/pic_transactions_112.svg', () => 'pic_transactions_112.svg')
+
+jest.mock('i18n/lingui', () => ({
+  i18n: {
+    _: (
+      key,
+      params,
+      data,
+    ) => {
+      if (!data) {
+        return key
+      }
+
+      const source = data.defaults
+
+      if (!params) {
+        return source
+      }
+
+      return Object.keys(params).reduce((
+        result,
+        param,
+      ) => {
+        if (result.indexOf(param) === -1) {
+          return result
+        }
+
+        return result.replace(`{${param}}`, params[param])
+      }, source)
+    }
+  }
+}))
